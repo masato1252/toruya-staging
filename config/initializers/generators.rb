143b5920f67f16eb3bc9b609de1e0ba5281241a8
ilake <lake.ilakela+github@gmail.com>
2016-07-04 22:41:18 +0800

    skip generator options

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.template_engine nil #to skip views
+  g.test_framework  nil #to skip test framework
+  g.assets  false
+  g.helper false
+  g.stylesheets false
+end
+
