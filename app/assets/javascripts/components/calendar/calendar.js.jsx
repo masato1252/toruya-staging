7a16598b8532e1c2f2ba1e5402c5bd8ea4fd7325
ilake <lake.ilakela+github@gmail.com>
2016-07-07 20:58:35 +0800

    tweak code

--- app/assets/javascripts/components/calendar/calendar.js.jsx
+++ app/assets/javascripts/components/calendar/calendar.js.jsx
@@ -30,39 +30,35 @@ UI.define("Calendar", function() {
 
     render: function() {
       return <div>
-      <div className="header">
-        <i className="fa fa-angle-left" onClick={this.previous}></i>
-          {this.renderMonthLabel()}
-        <i className="fa fa-angle-right" onClick={this.next}></i>
-      </div>
-      <UI.DayNames dayNames={this.props.dayNames} />
-      {this.renderWeeks()}
-      </div>;
+              <div className="header">
+                <i className="fa fa-angle-left" onClick={this.previous}></i>
+                  <span>{this.state.month.format("MMMM, YYYY")}</span>
+                <i className="fa fa-angle-right" onClick={this.next}></i>
+              </div>
+              <UI.DayNames dayNames={this.props.dayNames} />
+              {this.renderWeeks()}
+             </div>;
     },
 
     renderWeeks: function() {
       var weeks = [],
-        done = false,
+          done = false,
           date = this.state.month.clone().startOf("month").add("w" -1).day("Sunday"),
-            monthIndex = date.month(),
-              count = 0;
+          monthIndex = date.month(),
+          count = 0;
 
-              while (!done) {
-                weeks.push(<UI.Week key={date.toString()}
-                                    date={date.clone()}
-                                    month={this.state.month}
-                                    select={this.select}
-                                    selected={this.state.month} />);
-                date.add(1, "w");
-                done = count++ > 2 && monthIndex !== date.month();
-                monthIndex = date.month();
-              }
+          while (!done) {
+            weeks.push(<UI.Week key={date.toString()}
+                                date={date.clone()}
+                                month={this.state.month}
+                                select={this.select}
+                                selected={this.state.month} />);
+            date.add(1, "w");
+            done = count++ > 2 && monthIndex !== date.month();
+            monthIndex = date.month();
+          }
 
-              return weeks;
-    },
-
-    renderMonthLabel: function() {
-      return <span>{this.state.month.format("MMMM, YYYY")}</span>;
+          return weeks;
     }
   });
 
