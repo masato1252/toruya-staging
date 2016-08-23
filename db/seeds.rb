79b5ab396d8b96332abd5947c257a4047eff27a3
ilake <lake.ilakela+github@gmail.com>
2016-08-23 19:54:44 +0800

    dev data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = FactoryGirl.create(:user, email: "superadmin@email.com", password: "password123", confirmed_at: Time.now)
+shop = FactoryGirl.create(:shop, user: user)
+menu = FactoryGirl.create(:menu, shop: shop, max_seat_number: nil)
+lecture_menu = FactoryGirl.create(:menu, :lecture, shop: shop)
+staff = FactoryGirl.create(:staff, shop: shop)
+staff2 = FactoryGirl.create(:staff, shop: shop)
+FactoryGirl.create(:reservation_setting, menu: menu)
+FactoryGirl.create(:reservation_setting, menu: lecture_menu)
+FactoryGirl.create(:staff_menu, staff: staff, menu: menu, max_customers: 2)
+FactoryGirl.create(:staff_menu, staff: staff2, menu: menu, max_customers: 2)
+FactoryGirl.create(:staff_menu, staff: staff, menu: lecture_menu, max_customers: nil)
+FactoryGirl.create(:staff_menu, staff: staff2, menu: lecture_menu, max_customers: nil)
+
+4.times do
+  FactoryGirl.create(:customer, shop: shop)
+end
+
+(1..5).each do |wday|
+  FactoryGirl.create(:business_schedule, shop: shop, day_of_week: wday)
+end
+
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "foo#{n}@gmail.com" }
     password "foobar78"
+    confirmed_at { Time.now }
   end
 
   factory :shop do
@@ -23,18 +24,31 @@ FactoryGirl.define do
     association :shop
     day_of_week { start_time.wday }
     business_state "opened"
+    start_time { Time.local(2016, 8, 22, 3, 0, 0) }
+    end_time { Time.local(2016, 8, 22, 20, 0, 0) }
   end
 
   factory :menu do
     association :shop
-    name "foo"
-    shortname "f"
+    sequence(:name) { |n| "menu-#{n}" }
+    sequence(:shortname) { |n| "m-#{n}" }
+    minutes 60
+    min_staffs_number 1
+
+    trait :lecture do
+      min_staffs_number 2
+      max_seat_number 3
+    end
+
+    trait :easy do
+      min_staffs_number nil
+    end
   end
 
   factory :reservation_setting do
     association :menu
-    name "foo"
-    short_name "f"
+    sequence(:name) { |n| "settings-#{n}" }
+    sequence(:short_name) { |n| "s-#{n}" }
     day_type "business_days"
 
     trait :weekly do
@@ -65,14 +79,20 @@ FactoryGirl.define do
 
   factory :staff do
     association :shop
-    name "foo"
-    shortname "f"
+    sequence(:name) { |n| "staff-#{n}" }
+    sequence(:shortname) { |n| "s-#{n}" }
     full_time true
+  end
+
+  factory :staff_menu do
+    association :staff
+    association :menu
+    max_customers 2
   end
 
   factory :customer do
     association :shop
-    last_name "last_name"
-    first_name "first_name"
+    sequence(:last_name) { |n| "last_name-#{n}" }
+    sequence(:first_name) { |n| "first_name-#{n}" }
   end
 end
