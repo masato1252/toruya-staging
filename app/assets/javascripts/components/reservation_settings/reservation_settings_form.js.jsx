0b844b9bb67882b101d14ddd66cc310fa4c07259
ilake <lake.ilakela+github@gmail.com>
2016-09-10 11:34:38 +0800

    tweak reservation setting

--- app/assets/javascripts/components/reservation_settings/reservation_settings_form.js.jsx
+++ app/assets/javascripts/components/reservation_settings/reservation_settings_form.js.jsx
@@ -23,19 +23,35 @@ UI.define("ReservationSettings.Form", function() {
 
     render: function() {
       return (
-        <form>
+        <form acceptCharset="UTF-8" action={this.props.saveSettingPath} method="post">
+          <input name="utf8" type="hidden" value="✓" />
+          { this.props.setting.id ?  <input name="_method" type="hidden" value="PUT" /> : null }
+          <input name="authenticity_token" type="hidden" value={this.props.formAuthenticityToken} />
+
           <h3 className="frameInfo">予約枠 Informations<strong>必須項目</strong></h3>
           <div id="frameInfo" className="formRow">
             <dl>
               <dt>枠名</dt>
               <dd>
-                <input type="text" placeholder="予約枠 Name" maxlength="30" defaultValue={this.state.setting.name}/>
+                <input
+                  type="text"
+                  placeholder="予約枠 Name"
+                  maxlength="30"
+                  name="reservation_setting[name]"
+                  defaultValue={this.state.setting.name}
+                  />
               </dd>
             </dl>
             <dl>
               <dt>短縮名</dt>
               <dd>
-                <input type="text" placeholder="予約枠 Shorten Name" maxlength="10" defaultValue={this.state.setting.short_name} />
+                <input
+                  type="text"
+                  placeholder="予約枠 Shorten Name"
+                  maxlength="10"
+                  name="reservation_setting[short_name]"
+                  defaultValue={this.state.setting.short_name}
+                />
               </dd>
             </dl>
           </div>
@@ -71,6 +87,7 @@ UI.define("ReservationSettings.Form", function() {
               onChange={this.swithDayType}
               />
             <label htmlFor="frameDay3"><span>Monthly</span></label>
+            <input type="hidden" name="reservation_setting[day_type]" value={this.state.setting.day_type} />
           </div>
 
           {
@@ -84,11 +101,17 @@ UI.define("ReservationSettings.Form", function() {
                       { this.props.dayNames.map(function(dayName, i) {
                           return (
                             <div>
-                              <input type="checkbox" name="frameDate" id={`frameDate${i}`} />
+                              <input
+                                type="checkbox"
+                                name="reservation_setting[days_of_week][]"
+                                defaultChecked={_.contains(this.state.setting.days_of_week, `${i}`)}
+                                value={i}
+                                id={`frameDate${i}`}
+                              />
                               <label htmlFor={`frameDate${i}`}><span>{dayName}</span></label>
                             </div>
                           )
-                        })}
+                        }.bind(this))}
                     </div>
                   </dd>
                 </dl>
@@ -105,14 +128,30 @@ UI.define("ReservationSettings.Form", function() {
                     <input type="radio" name="frameDayMonthly" id="frameDayMonthly1" checked="" />
                     <label htmlFor="frameDayMonthly1">Number of the day</label>
                   </dt>
-                  <dd>毎月<input type="number" size="2" maxlength="2" />日</dd>
+                  <dd>毎月
+                    <input
+                      type="number"
+                      size="2"
+                      maxlength="2"
+                      name="reservation_setting[day]"
+                      defaultValue={this.state.setting.day}
+                    />
+                      日
+                    </dd>
                 </dl>
                 <dl>
                   <dt>
-                    <input type="radio" name="frameDayMonthly" id="frameDayMonthly2" />
                     <label htmlFor="frameDayMonthly2">Number of the week</label>
                   </dt>
-                  <dd>毎月第<input type="number" size="1" maxlength="1" /></dd>
+                  <dd>毎月第
+                    <input
+                      type="number"
+                      size="1"
+                      maxlength="1"
+                      name="reservation_setting[nth_of_week]"
+                      defaultValue={this.state.setting.nth_of_week}
+                      />
+                  </dd>
                 </dl>
                 <dl>
                   <dt>Day of the week</dt>
@@ -121,11 +160,16 @@ UI.define("ReservationSettings.Form", function() {
                       { this.props.dayNames.map(function(dayName, i) {
                           return (
                             <div>
-                              <input type="checkbox" name="frameDayMonthlyWeekday" id={`frameDayMonthlyWeekday${i}`} />
+                              <input
+                                type="checkbox"
+                                name="reservation_setting[days_of_week][]"
+                                value={i}
+                                defaultChecked={_.contains(this.state.setting.days_of_week, `${i}`)}
+                                id={`frameDayMonthlyWeekday${i}`} />
                               <label htmlFor={`frameDayMonthlyWeekday${i}`}><span>{dayName}</span></label>
                             </div>
                           )
-                        })}
+                        }.bind(this))}
                     </div>
                   </dd>
                 </dl>
@@ -159,19 +203,41 @@ UI.define("ReservationSettings.Form", function() {
               <div className="setDetail formRow">
                 <dl>
                   <dt>Starts from</dt>
-                  <dd><input type="time" /></dd>
+                  <dd>
+                    <input
+                      type="time"
+                      name="reservation_setting[start_time]"
+                      defaultValue={moment(this.state.setting.start_time).format("HH:mm")}
+                      />
+                  </dd>
                 </dl>
                 <dl>
                   <dt>Ends at</dt>
-                  <dd><input type="time" /></dd>
+                  <dd>
+                    <input
+                      type="time"
+                      name="reservation_setting[end_time]"
+                      defaultValue={moment(this.state.setting.end_time).format("HH:mm")}
+                      />
+                  </dd>
                 </dl>
               </div>
             )
           }
 
           <ul id="footerav">
-            <li><a href="menu.html" className="BTNtarco">Cancel</a></li>
-            <li><a href="menu.html" className="BTNyellow">保存</a></li>
+            <li>
+              <a className="BTNtarco" href={this.props.cancelPath}>Cancel</a>
+            </li>
+            <li>
+              <input
+                type="submit"
+                name="commit"
+                value="保存"
+                className="BTNyellow"
+                data-disable-with="保存"
+                />
+              </li>
           </ul>
         </form>
       );
--- app/controllers/settings/reservation_settings_controller.rb
+++ app/controllers/settings/reservation_settings_controller.rb
@@ -1,9 +1,48 @@
 class Settings::ReservationSettingsController < SettingsController
+  before_action :set_reservation_setting, only: [:edit, :update, :destroy]
+
   def index
     @reservation_settings = super_user.reservation_settings
   end
 
   def new
     @reservation_setting = super_user.reservation_settings.new
+  end
+
+  def create
+    @reservation_setting = super_user.reservation_settings.new(reservation_setting_params)
+    if @reservation_setting.save
+      redirect_to settings_reservation_settings_path
+    else
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @reservation_setting.update(reservation_setting_params)
+        format.html { redirect_to settings_reservation_settings_path, notice: 'Menu was successfully updated.' }
+        format.json { render :show, status: :ok, location: @menu }
+      else
+        format.html { render :edit }
+        format.json { render json: @settings_menu.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation_setting
+    @reservation_setting = super_user.reservation_settings.find(params[:id])
+  end
+
+  def reservation_setting_params
+    params.require(:reservation_setting).permit(
+      :id, :name, :short_name, :day_type,
+      :day, :nth_of_week, :start_time, :end_time, days_of_week: []
+    )
   end
 end
