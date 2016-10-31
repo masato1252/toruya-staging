4764eae2810de71c96bb082eb0530f1b03519217
ilake <lake.ilakela+github@gmail.com>
2016-10-31 22:44:49 +0800

    Delete legacy contact data when switch google account

--- app/interactions/create_google_oauth.rb
+++ app/interactions/create_google_oauth.rb
@@ -5,12 +5,20 @@ class CreateGoogleOauth < ActiveInteraction::Base
   def execute
     data = auth.info
 
+    if user.access_provider && user.access_provider.uid != auth.uid
+      user_provider_account_changed = true
+    end
+
     access = user.access_provider || user.build_access_provider
     access.provider = auth.provider
     access.uid = auth.uid
     access.access_token = auth.credentials.token
     access.refresh_token = auth.credentials.refresh_token
-    access.save
+
+    if access.save && user_provider_account_changed
+      Customer.where(user: user).delete_all
+      ContactGroup.where(user: user).delete_all
+    end
 
     access
   end
