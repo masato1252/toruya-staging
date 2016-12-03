a4b2d5b3a5372a7dcb42411546ed8b9674dbfa4a
ilake <lake.ilakela+github@gmail.com>
2016-12-03 13:36:53 +0800

    tweak

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -29,4 +29,5 @@ class Profile < ApplicationRecord
   validates :company_name, presence: true
   validates :zip_code, presence: true
   validates :address, presence: true
+  validates :phone_number, presence: true
 end
