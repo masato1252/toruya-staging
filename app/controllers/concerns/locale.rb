68036ac19d22acad9d1c5321636db8eb303a866b
ilake <lake.ilakela+github@gmail.com>
2016-12-05 21:17:45 +0800

    switch locale

--- app/controllers/concerns/locale.rb
+++ app/controllers/concerns/locale.rb
@@ -6,6 +6,7 @@ module Locale
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    session[:locale] = params[:locale] if params[:locale]
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end
