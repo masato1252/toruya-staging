daaebf8f68fef74944ddc649865a96b4c8522a0b
ilake <lake.ilakela+github@gmail.com>
2016-12-28 23:29:40 +0800

    Fix staff custom schedule

--- app/controllers/settings/working_time/staffs_controller.rb
+++ app/controllers/settings/working_time/staffs_controller.rb
@@ -38,6 +38,6 @@ class Settings::WorkingTime::StaffsController < SettingsController
   end
 
   def custom_schedules_params
-    params.permit(custom_schedules: [:id, :start_time_date_part, :start_time_time_part, :end_time, :reason, :_destroy])
+    params.permit(custom_schedules: [:id, :start_time_date_part, :start_time_time_part, :end_time_time_part, :reason, :_destroy])
   end
 end
