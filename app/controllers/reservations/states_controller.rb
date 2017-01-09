389c58189f42c194ad5030fc6b99d66d39fecb12
ilake <lake.ilakela+github@gmail.com>
2017-01-09 17:43:48 +0800

    change successfully

--- app/controllers/reservations/states_controller.rb
+++ app/controllers/reservations/states_controller.rb
@@ -1,27 +1,27 @@
 class Reservations::StatesController < DashboardController
   def pend
     reservation.pend!
-    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: "Change Successfully"
+    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("common.create_or_update_successfully_message")
   end
 
   def accept
     reservation.accept!
-    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: "Change Successfully"
+    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("common.create_or_update_successfully_message")
   end
 
   def check_in
     reservation.check_in!
-    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: "Change Successfully"
+    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("common.create_or_update_successfully_message")
   end
 
   def check_out
     reservation.check_out!
-    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: "Change Successfully"
+    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("common.create_or_update_successfully_message")
   end
 
   def cancel
     reservation.destroy
-    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: "Change Successfully"
+    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("common.create_or_update_successfully_message")
   end
 
   private
