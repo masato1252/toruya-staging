ce81d29fc58ac8262e76be85e5708f131524e1c7
ilake <lake.ilakela+github@gmail.com>
2017-01-14 21:20:27 +0800

    Check production protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  if ENV["HTTP_PROTOCOL"]
+  if ENV["HTTP_PROTOCOL"] == "https"
     config.force_ssl = true
   end
 
