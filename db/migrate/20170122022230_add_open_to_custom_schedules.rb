ea39bf24a4a8af5b7a2ca400143601b6148ae81d
ilake <lake.ilakela+github@gmail.com>
2017-01-22 12:17:00 +0800

    Add open column to custom schedule

---
+++ db/migrate/20170122022230_add_open_to_custom_schedules.rb
@@ -0,0 +1,6 @@
+class AddOpenToCustomSchedules < ActiveRecord::Migration[5.0]
+  def change
+    add_column :custom_schedules, :open, :boolean, default: false, null: false
+    add_index :custom_schedules, [:staff_id, :open]
+  end
+end
