f1dea54d06d118408b9a218aacad6fb3c7fb057d
ilake <lake.ilakela+github@gmail.com>
2017-05-23 19:56:57 +0800

    free member only can create one staff.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,8 +6,12 @@ class Ability
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
     if user.free_level?
+      if !user.staffs.exists?
+        can :create, Staff
+      end
     elsif user.basic_level? || user.premium_level?
       can :swith_staffs_selector, User
+      can :create, Staff
     end
   end
 end
