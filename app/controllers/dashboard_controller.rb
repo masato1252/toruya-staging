c82dcac62d5d63c522f546a95bfd48561be30a12
ilake <lake.ilakela+github@gmail.com>
2017-06-18 09:04:58 +0800

    Fix staff requirement order

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,11 @@
 class DashboardController < ActionController::Base
   layout "application"
   protect_from_forgery with: :exception, prepend: true
-  before_action :staff_requirement
-
   include ViewHelpers
   include Locale
   include Ssl
+
+  before_action :staff_requirement
 
   rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound do
     redirect_to root_path, :alert => "This page does not exist."
