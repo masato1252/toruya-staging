a826767dcceb4e27ad07e3a17616aadc6247849a
ilake <lake.ilakela+github@gmail.com>
2017-09-03 11:33:33 +0800

    A message after canceled a reservation should be changed to ”予約がキャンセルされました。"

--- app/controllers/reservations/states_controller.rb
+++ app/controllers/reservations/states_controller.rb
@@ -21,7 +21,7 @@ class Reservations::StatesController < DashboardController
 
   def cancel
     reservation.cancel!
-    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("reservation.delete_successfully_message")
+    redirect_to shop_reservations_path(shop, reservation_date: params[:reservation_date]), notice: I18n.t("reservation.canceled_successfully_message")
   end
 
   private
