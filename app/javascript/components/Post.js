588d94dc6c186a781294337aedd0ae56b0726e42
ilake <lake.ilakela+github@gmail.com>
2017-11-06 22:46:21 +0800

    Make Post.js work

---
+++ app/javascript/components/Post.js
@@ -0,0 +1,31 @@
+// import React from "react"
+// import PropTypes from "prop-types"
+//
+// var HelloWorld = createReactClass({
+//   propTypes: {
+//     greeting: PropTypes.string
+//   },
+//
+//   render: function() {
+//     return (
+//       <div>
+//         <div>Greeting: {this.props.greeting}</div>
+//       </div>
+//     );
+//   }
+// });
+// export default HelloWorld
+// import Paper from "./posts/paper.js"
+import React from 'react';
+
+class Post extends React.Component {
+  render () {
+    return (
+      <div>
+        <div>Hello</div>
+      </div>
+    );
+  }
+}
+
+export default Post;
