8c0da97391d806936f8cf8f0577b416d34b06ec0
ilake <lake.ilakela+github@gmail.com>
2018-03-27 15:14:31 +0800

    We don't need to have shop obj

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -4,10 +4,5 @@ module Authorization
   included do
     protect_from_forgery prepend: true, with: :exception
     before_action :authenticate_user!
-    before_action :authenticate_shop_permission!
-  end
-
-  def authenticate_shop_permission!
-    authorize! :read, shop
   end
 end
