50ae4e055e89ee1a006d37efd446f17837b32bcb
ilake <lake.ilakela+github@gmail.com>
2018-03-27 21:42:21 +0800

    redirect_to member page when login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,4 @@ class DashboardController < ActionController::Base
   include ViewHelpers
   include Locale
   include ExceptionHandler
-
-  before_action :staff_requirement
-
-  def staff_requirement
-    unless staff
-      redirect_to settings_path(current_user)
-    end
-  end
 end
