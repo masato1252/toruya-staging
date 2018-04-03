eb0418d39bc71489980be3af7f582c1ae03b5e8a
ilake <lake.ilakela+github@gmail.com>
2018-04-03 23:38:12 +0800

    Downgrade shop_closed error => warning

--- app/javascript/components/reservations/reservation_form.js
+++ app/javascript/components/reservations/reservation_form.js
@@ -13,8 +13,8 @@ UI.define("Reservation.Form", function() {
     static errorGroups() {
       return (
         {
-          errors: ["shop_closed", "unworking_staff", "time_not_enough", "start_yet", "is_over"],
-          warnings: ["interval_too_short", "overlap_reservations", "other_shop", "incapacity_menu", "unschedule_menu",
+          errors: ["unworking_staff", "time_not_enough", "start_yet", "is_over"],
+          warnings: ["shop_closed", "interval_too_short", "overlap_reservations", "other_shop", "incapacity_menu", "unschedule_menu",
                      "not_enough_seat", "not_enough_ability"],
           menu_errors: ["time_not_enough", "not_enough_seat", "unschedule_menu", "start_yet", "is_over"],
           menu_danger_errors: ["start_yet", "is_over"],
