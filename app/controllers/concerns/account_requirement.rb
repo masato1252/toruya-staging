cca68673683d5a3e255923a1aee2c90bdbc1d2f9
ilake <lake.ilakela+github@gmail.com>
2018-04-13 22:19:34 +0800

    The setting requirement checking should base on different super user.

--- app/controllers/concerns/account_requirement.rb
+++ app/controllers/concerns/account_requirement.rb
@@ -16,13 +16,13 @@ module AccountRequirement
 
   def check_requirement
     if is_owner
-      if !session[:user_name_checking]
+      if !session["user_name_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last)
           flash[:alert] = I18n.t("requirement.profile_redirect_message")
 
           redirect_to new_settings_user_profile_path(super_user)
         end
-      elsif !session[:contact_checking]
+      elsif !session["contact_checking_#{super_user.id}"]
         # Allow user goes to the path that he already fit the restriction. Otherwise redirect to the proper restriction path.
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last(2))
           flash[:alert] = I18n.t("requirement.contact_redirect_message").html_safe
@@ -32,7 +32,7 @@ module AccountRequirement
         elsif except_path("settings/contact_groups")
           flash.now[:alert] = I18n.t("requirement.contact_warning_message", link: view_context.link_to(I18n.t("requirement.contact_warning_link_title"), settings_user_contact_groups_path(super_user))).html_safe
         end
-      elsif !session[:shop_checking]
+      elsif !session["shop_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last(3))
           flash[:alert] = I18n.t("requirement.shop_redirect_message")
 
@@ -40,7 +40,7 @@ module AccountRequirement
         elsif except_path("settings/shops")
           flash.now[:alert] = I18n.t("requirement.shop_warning_message", link: view_context.link_to(I18n.t("requirement.shop_warning_link_title"), new_settings_user_shop_path(super_user))).html_safe
         end
-      elsif !session[:business_hours_checking]
+      elsif !session["business_hours_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last(4))
           flash[:alert] = I18n.t("requirement.business_schedule_redirect_message")
 
@@ -48,7 +48,7 @@ module AccountRequirement
         elsif except_path("business_schedules")
           flash.now[:alert] = I18n.t("requirement.business_schedule_warning_message", link: view_context.link_to(I18n.t("requirement.business_schedule_warning_link_title"), settings_user_business_schedules_path(super_user))).html_safe
         end
-      elsif !session[:staffs_checking]
+      elsif !session["staffs_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last(5))
           flash[:alert] = I18n.t("requirement.staff_redirect_message")
 
@@ -56,7 +56,7 @@ module AccountRequirement
         elsif except_path("settings/staffs")
           flash.now[:alert] = I18n.t("requirement.staff_warning_message", link: view_context.link_to(I18n.t("requirement.staff_warning_link_title"), new_settings_user_staff_path(super_user))).html_safe
         end
-      elsif !session[:working_time_checking]
+      elsif !session["working_time_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last(6))
           flash[:alert] = I18n.t("requirement.staff_working_schedule_redirect_message")
 
@@ -64,7 +64,7 @@ module AccountRequirement
         elsif except_path("settings/working_time/staffs")
           flash.now[:alert] = I18n.t("requirement.staff_working_schedule_warning_message", link: view_context.link_to(I18n.t("requirement.staff_working_schedule_warning_link_title"), settings_user_working_time_staffs_path(super_user, mode: "working_schedules"))).html_safe
         end
-      elsif !session[:reservation_settings_checking]
+      elsif !session["reservation_settings_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS.last(7))
           flash[:alert] = I18n.t("requirement.reservation_setting_redirect_message")
 
@@ -72,7 +72,7 @@ module AccountRequirement
         elsif except_path("settings/reservation_settings")
           flash.now[:alert] = I18n.t("requirement.reservation_setting_warning_message", link: view_context.link_to(I18n.t("requirement.reservation_setting_warning_link_title"), new_settings_user_reservation_setting_path(super_user))).html_safe
         end
-      elsif !session[:menu_checking]
+      elsif !session["menu_checking_#{super_user.id}"]
         if except_path(ALLOWED_ACCESS_CONTROLLERS)
           flash[:alert] = I18n.t("requirement.menu_redirect_message")
 
@@ -85,50 +85,50 @@ module AccountRequirement
   end
 
   def require_user_name
-    if !session[:user_name_checking] && is_owner && super_user.name
-      session[:user_name_checking] = true
+    if !session["user_name_checking_#{super_user.id}"] && is_owner && super_user.name
+      session["user_name_checking_#{super_user.id}"] = true
     end
   end
 
   def require_contacts
-    if !session[:contact_checking] && is_owner && super_user.uid && super_user.contact_groups.connected.exists?
-      session[:contact_checking] = true
+    if !session["contact_checking_#{super_user.id}"] && is_owner && super_user.uid && super_user.contact_groups.connected.exists?
+      session["contact_checking_#{super_user.id}"] = true
     end
   end
 
   def require_shop
-    if !session[:shop_checking] && is_owner && super_user.shops.exists?
-      session[:shop_checking] = true
+    if !session["shop_checking_#{super_user.id}"] && is_owner && super_user.shops.exists?
+      session["shop_checking_#{super_user.id}"] = true
     end
   end
 
   def require_business_hours
-    if !session[:business_hours_checking] && is_owner && BusinessSchedule.where(shop_id: super_user.shop_ids).exists?
-      session[:business_hours_checking] = true
+    if !session["business_hours_checking_#{super_user.id}"] && is_owner && BusinessSchedule.where(shop_id: super_user.shop_ids).exists?
+      session["business_hours_checking_#{super_user.id}"] = true
     end
   end
 
   def require_staffs
-    if !session[:staffs_checking] && is_owner && super_user.staffs.exists?
-      session[:staffs_checking] = true
+    if !session["staffs_checking_#{super_user.id}"] && is_owner && super_user.staffs.exists?
+      session["staffs_checking_#{super_user.id}"] = true
     end
   end
 
   def require_working_times
-    if !session[:working_time_checking] && is_owner && BusinessSchedule.where(staff_id: super_user.staff_ids).exists?
-      session[:working_time_checking] = true
+    if !session["working_time_checking_#{super_user.id}"] && is_owner && BusinessSchedule.where(staff_id: super_user.staff_ids).exists?
+      session["working_time_checking_#{super_user.id}"] = true
     end
   end
 
   def require_reservation_settings
-    if !session[:reservation_settings_checking] && is_owner && super_user.reservation_settings.exists?
-      session[:reservation_settings_checking] = true
+    if !session["reservation_settings_checking_#{super_user.id}"] && is_owner && super_user.reservation_settings.exists?
+      session["reservation_settings_checking_#{super_user.id}"] = true
     end
   end
 
   def require_menu
-    if !session[:menu_checking] && is_owner && super_user.menus.exists?
-      session[:menu_checking] = true
+    if !session["menu_checking_#{super_user.id}"] && is_owner && super_user.menus.exists?
+      session["menu_checking_#{super_user.id}"] = true
     end
   end
 
