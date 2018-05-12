d5da7cd1d242da6b0cecc3f00a39161e6ca9beee
ilake <lake.ilakela+github@gmail.com>
2018-05-13 07:38:02 +0800

    Enable source-map for rollbar to track js error
    https://docs.rollbar.com/docs/source-maps/
    https://github.com/rails/webpacker/pull/770

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,9 @@
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+// it was disabled by default in PR https://github.com/rails/webpacker/pull/770
+// https://github.com/rails/webpacker/issues/769#issuecomment-328452033
+// enable sourcemap
+const config = environment.toWebpackConfig()
+config.devtool = 'source-map'
+
+module.exports = config
