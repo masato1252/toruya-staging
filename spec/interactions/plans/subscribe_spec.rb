d68e2f0372e422e7db5724628fa3f6b12569f755
ilake <lake.ilakela+github@gmail.com>
2018-06-21 21:20:10 +0800

    Add spec

---
+++ spec/interactions/plans/subscribe_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe Plans::Subscribe do
+  describe "#execute" do
+    context "when users don't have a subscription yet" do
+    end
+
+    context "when users had a subscription" do
+    end
+  end
+end
