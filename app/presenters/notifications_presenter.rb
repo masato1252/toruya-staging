6e284849bc7997f539d333d891d5328dbd9e639e
ilake <lake.ilakela+github@gmail.com>
2018-06-27 18:06:10 +0800

    Give shop data when user try to edit staff

--- app/presenters/notifications_presenter.rb
+++ app/presenters/notifications_presenter.rb
@@ -22,7 +22,7 @@ class NotificationsPresenter
 
   def new_staff_accounts
     current_user.staffs.active_without_data.includes(:staff_account).map do |staff|
-      "#{I18n.t("settings.staff_account.new_staff_active")} #{view_context.link_to(I18n.t("settings.staff_account.staff_setting"), view_context.edit_settings_user_staff_path(current_user, staff))}"
+      "#{I18n.t("settings.staff_account.new_staff_active")} #{view_context.link_to(I18n.t("settings.staff_account.staff_setting"), view_context.edit_settings_user_staff_path(current_user, staff, shop_id: current_user.shop_ids.first))}"
     end
   end
 end
