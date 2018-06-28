34e13096895ac54ce89feb2def7dbc08302b1274
ilake <lake.ilakela+github@gmail.com>
2018-06-29 01:03:52 +0800

    Only import bootstrap when we need it.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,5 +20,3 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
-
-import "bootstrap"
