cf279640622387fd6a8931a4145078b01ed91dd8
ilake <lake.ilakela+github@gmail.com>
2018-07-03 10:14:49 +0800

    Add charge reminder spec

--- spec/tasks/subscriptions_spec.rb
+++ spec/tasks/subscriptions_spec.rb
@@ -69,3 +69,59 @@ RSpec.describe "rake subscriptions:charge" do
     end
   end
 end
+
+RSpec.describe "rake subscriptions:charge_reminder" do
+  before do
+    Time.zone = "Tokyo"
+    Timecop.freeze(today)
+  end
+
+  let!(:subscription) { FactoryBot.create(:subscription, :basic, expired_date: expired_date, recurring_day: recurring_day) }
+
+  context "when today is 7 days ago before subscription's expired date" do
+    let(:expired_date) { Date.new(2018, 2, 28) }
+    let(:recurring_day) { 31 }
+    let(:today) { Date.new(2018, 2, 21) }
+
+    it "reminds users" do
+      expect(SubscriptionMailer).to receive(:charge_reminder).with(subscription).and_return(double(deliver_later: true))
+
+      task.execute
+    end
+
+    context "when user subscribe free plan" do
+      let!(:subscription) { FactoryBot.create(:subscription, :free, expired_date: expired_date, recurring_day: recurring_day) }
+
+      it "does nothing" do
+        expect(SubscriptionMailer).not_to receive(:charge_reminder)
+
+        task.execute
+      end
+    end
+  end
+
+  context "when today is not equal 7 days ago before subscription's expired date" do
+    let(:expired_date) { Date.new(2018, 2, 28) }
+    let(:recurring_day) { 31 }
+
+    context "when today 8 days ago before expired date" do
+      let(:today) { Date.new(2018, 2, 20) }
+
+      it "does nothing" do
+        expect(SubscriptionMailer).not_to receive(:charge_reminder)
+
+        task.execute
+      end
+    end
+
+    context "when today 6 days ago before expired date" do
+      let(:today) { Date.new(2018, 2, 22) }
+
+      it "does nothing" do
+        expect(SubscriptionMailer).not_to receive(:charge_reminder)
+
+        task.execute
+      end
+    end
+  end
+end
