f9e861280ebc90a24d11a29abb3db218991c3d8a
ilake <lake.ilakela+github@gmail.com>
2018-07-17 18:25:45 +0800

    tmp

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
 const erb =  require('./loaders/erb')
 
-environment.loaders.append('erb', erb)
+environment.loaders.set('erb', erb)
 module.exports = environment
