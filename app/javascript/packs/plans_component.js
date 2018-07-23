1e40b7cad76253b4969c7b2013f70ac52919c29a
ilake <lake.ilakela+github@gmail.com>
2018-07-23 17:18:51 +0800

    I18n for plan, payment

--- app/javascript/packs/plans_component.js
+++ app/javascript/packs/plans_component.js
@@ -5,6 +5,7 @@ import Plans from 'components/plans/plans.js';
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.querySelector('#plans')
   const data = JSON.parse(node.getAttribute('data'))
+  const i18n = JSON.parse(node.getAttribute('i18n'))
 
-  ReactDOM.render(<Plans {...data} />, node)
+  ReactDOM.render(<Plans {...data} i18n={i18n} />, node)
 })
