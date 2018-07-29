b1ad72fafecc34c7005ce894e83d418d107e2b04
ilake <lake.ilakela+github@gmail.com>
2018-07-29 21:56:19 +0800

    Use new syntax

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
 const erb =  require('./loaders/erb')
 
-environment.loaders.set('erb', erb)
+environment.loaders.prepend('erb', erb)
 module.exports = environment
