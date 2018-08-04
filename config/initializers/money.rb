b3ef91b0c6539f6ea99c00b44e6c1fb16b7065b7
ilake <lake.ilakela+github@gmail.com>
2018-08-04 16:54:48 +0800

    Handle select plan behavior.

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -2,3 +2,20 @@ MoneyRails.configure do |config|
   # set the default currency
   config.default_currency = :jpy
 end
+
+class Money
+  class FormattingRules
+    def localize_formatting_rules(rules)
+      if rules[:ja_default_format]
+        super
+      else
+        if currency.iso_code == "JPY" && I18n.locale == :ja
+          rules[:symbol] = "¥"
+          rules[:symbol_position] = :before
+          rules[:symbol_before_without_space] = true
+        end
+        rules
+      end
+    end
+  end
+end
