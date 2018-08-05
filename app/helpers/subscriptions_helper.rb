36c0caa5f3a24a865ec810a9488f35c16999a118
ilake <lake.ilakela+github@gmail.com>
2018-08-05 21:30:25 +0800

    Handle downgrade flow.

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,9 +1,15 @@
 module SubscriptionsHelper
   def subscription_status(subscription)
-    if subscription.active?
+    sentence = if subscription.active?
       "Active"
     else
       "#{subscription.plan.name} expired"
     end
+
+    if subscription.next_plan
+      sentence << " (#{subscription.next_plan.name} from #{l(subscription.expired_date.tomorrow)})"
+    end
+
+    sentence
   end
 end
