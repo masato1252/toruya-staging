7b387e2114c36cd402ab0b095c5dc0adcb981ded
ilake <lake.ilakela+github@gmail.com>
2018-08-07 20:53:11 +0800

    translation for receipt

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,13 +1,7 @@
 module SubscriptionsHelper
   def subscription_status(subscription)
     if subscription.active?
-      status = t("settings.subscription.status.active")
-
-      if subscription.next_plan
-        status << " (#{subscription.next_plan.name} from #{l(subscription.expired_date.tomorrow)})"
-      end
-
-      status
+      t("settings.subscription.status.active")
     else
       "#{subscription.plan.name} expired"
     end
