439f31bc38d8ee6e77bbe19180d69edddaf426f0
ilake <lake.ilakela+github@gmail.com>
2018-08-23 22:56:12 +0800

    Fix money format rule overwrite

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -5,9 +5,9 @@ end
 
 class Money
   class FormattingRules
-    def localize_formatting_rules(rules)
+    def custom_localize_formatting_rules(rules)
       if rules[:ja_default_format]
-        super
+        default_localize_formatting_rules(rules)
       else
         if currency.iso_code == "JPY" && I18n.locale == :ja
           rules[:symbol] = "¥" unless rules[:symbol] == false
@@ -17,5 +17,8 @@ class Money
         rules
       end
     end
+
+    alias_method :default_localize_formatting_rules, :localize_formatting_rules
+    alias_method :localize_formatting_rules, :custom_localize_formatting_rules
   end
 end
