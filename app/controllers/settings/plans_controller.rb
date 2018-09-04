4a6dcd8de8fdb97c57743a677c5f3239bd4befe5
ilake <lake.ilakela+github@gmail.com>
2018-09-04 09:38:32 +0800

    Upgrade button go to plan page.

--- app/controllers/settings/plans_controller.rb
+++ app/controllers/settings/plans_controller.rb
@@ -12,5 +12,6 @@ class Settings::PlansController < SettingsController
     @plan_labels = I18n.t("settings.plans")[:labels]
 
     @charge_directly = current_user.subscription.current_plan.free_level?
+    @default_upgrade_plan = params[:upgrade]
   end
 end
