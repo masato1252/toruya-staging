8bdea62a60271c37b8a4bbea247e3b946dfd621d
ilake <lake.ilakela+github@gmail.com>
2018-09-08 07:27:05 +0800

    Refactor shop create interaction, let compose trigger rollback.

--- app/interactions/shops/create.rb
+++ app/interactions/shops/create.rb
@@ -6,39 +6,19 @@ module Shops
 
     def execute
       Shop.transaction do
-        shop = user.shops.new(params)
-
-        if shop.valid?
-          if authorize_token.present?
-            charge = compose(Subscriptions::ShopFeeCharge, user: user, authorize_token: authorize_token)
-          end
-
-          shop.save
-          if charge
-            charge.details ||= {}
-            charge.details.merge!({
-              shop_ids: shop.id,
-              type: SubscriptionCharge::TYPES[:shop_fee]
-            })
-            charge.save!
-          end
-        end
+        shop = user.shops.create(params)
 
         if shop.new_record?
           errors.merge!(shop.errors)
         else
-          outcome = Staffs::CreateOwner.run(user: user)
+          if authorize_token.present?
+            compose(Subscriptions::ShopFeeCharge, user: user, shop: shop, authorize_token: authorize_token)
+          end
 
-          staff = outcome.result.staff
+          staff = compose(Staffs::CreateOwner, user: user).staff
 
           # Owner staff manage the same shops with User
           staff.shop_ids = Shop.where(user: user).pluck(:id)
-
-          if outcome.errors.present?
-            errors.merge!(outcome.errors)
-
-            raise ActiveRecord::Rollback
-          end
         end
 
         shop
