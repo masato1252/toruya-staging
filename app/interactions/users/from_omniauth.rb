4320bca0ff9167193359f643437ff452bd39f6a9
ilake <lake.ilakela+github@gmail.com>
2018-12-03 13:38:17 +0800

    Always use google account email as User one (#394)

--- app/interactions/users/from_omniauth.rb
+++ app/interactions/users/from_omniauth.rb
@@ -6,7 +6,7 @@ module Users
       user = (AccessProvider.where(provider: auth[:provider], uid: auth[:uid]).first.try(:user) ||
        User.where(email: auth.info.email).first_or_initialize
       )
-      user.email = user.email.presence || auth.info.email
+      user.email = auth.info.email.presence || user.email
       user.password = Devise.friendly_token[0,20]
       user.skip_confirmation!
       user.skip_confirmation_notification!
