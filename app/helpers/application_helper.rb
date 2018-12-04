03ac32fa9d845982e6cb7e9f37f7cb3bb5a50e00
ilake <lake.ilakela+github@gmail.com>
2018-12-04 19:02:16 +0800

    Fix flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,17 @@
 module ApplicationHelper
- BOOTSTRAP_FLASH_MSG = {
-    'success' => 'alert-success',
-    'error' => 'alert-danger',
-    'alert' => 'alert-warning',
-    'notice' => 'alert-info'
+ FLASH_MSG = {
+    'alert' => 'caution',
+    'notice' => 'alert'
   }
 
   def bootstrap_class_for(flash_type)
-    BOOTSTRAP_FLASH_MSG.fetch(flash_type, flash_type.to_s)
+    FLASH_MSG.fetch(flash_type, flash_type.to_s)
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       if message.present?
-        concat(content_tag(:div, message, :class => "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat(content_tag(:div, message, :class => "#{bootstrap_class_for(msg_type)} fade in") do
           concat content_tag(:button, 'x', :class => "close", :data => { :dismiss => 'alert' })
           concat message.html_safe
         end)
