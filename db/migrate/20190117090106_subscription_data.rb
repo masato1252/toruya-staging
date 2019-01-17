7f1d9327096489d30a90c06d06ee1f72ee9dc011
ilake <lake.ilakela+github@gmail.com>
2019-01-17 17:05:08 +0800

    migrate data

---
+++ db/migrate/20190117090106_subscription_data.rb
@@ -0,0 +1,12 @@
+class SubscriptionData < ActiveRecord::Migration[5.1]
+  def change
+    # https://github.com/ilake/kasaike/pull/360
+    User.find_each do |user|
+      Staffs::CreateOwner.run!(user: user) if user.profile
+    end
+
+    User.find_each do |user|
+      user.create_subscription(plan: Plan.free_level.take) unless user.subscription
+    end
+  end
+end
