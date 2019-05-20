5ee2cf8e9c571fb101c59ddc54b0d42a946bff75
ilake <lake.ilakela+github@gmail.com>
2019-05-20 23:01:59 +0800

    Fix component folder regex, the folders are like ./booking, ./shared, ./management

--- app/webpacker/packs/booking.js
+++ app/webpacker/packs/booking.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill'
 
-var BookingcomponentRequireContext = require.context("../javascripts/components", true, /booking|shared/)
+var BookingcomponentRequireContext = require.context("../javascripts/components", true, /^\.\/(booking|shared)/)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(BookingcomponentRequireContext)
 
