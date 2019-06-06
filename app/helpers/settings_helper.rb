28db6f164e10c37dae99b2e78e919e5aec043e80
ilake <lake.ilakela+github@gmail.com>
2019-06-06 15:54:22 +0800

    Don't use booking subdomain for now

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -8,7 +8,7 @@ module SettingsHelper
   end
 
   def booking_url(page)
-    @booking_url ||= Rails.configuration.x.env.staging? ? booking_page_url(page) : booking_page_url(page, subdomain: :booking)
+    booking_page_url(page)
   end
 
   def booking_page_share_button_link(page)
