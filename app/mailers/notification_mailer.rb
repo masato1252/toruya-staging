b2ff55b73e0bb28995eed93c85666ec9ea0ab01e
ilake <lake.ilakela+github@gmail.com>
2019-06-10 16:54:42 +0800

    Add duplicate customer mail template

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -44,11 +44,15 @@ class NotificationMailer < ApplicationMailer
          :subject => subject("スタッフが削除されました。"))
   end
 
-  def duplicate_customers(user, customers)
-    @user = user
+  def duplicate_customers(booking_page, customers, booking_customer, phone_number)
+    @booking_page = booking_page
+    @user = booking_page.user
     @customers = customers
+    @booking_customer = booking_customer
+    @phone_number = phone_number
 
     mail(:to => @user.email,
-         :subject => subject("Duplicate customers"))
+         subject: subject(I18n.t("notification_mailer.duplicate_customers.title")),
+         locale: I18n.default_locale)
   end
 end
