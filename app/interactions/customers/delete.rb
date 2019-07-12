a911f442a8faadd92300459774b632d4016c3c8c
ilake <lake.ilakela+github@gmail.com>
2019-07-12 21:39:17 +0800

    Add soft_delete option to delete customer for real

--- app/interactions/customers/delete.rb
+++ app/interactions/customers/delete.rb
@@ -1,10 +1,15 @@
 class Customers::Delete < ActiveInteraction::Base
   object :customer
+  boolean :soft_delete, default: true
 
   def execute
     # Google contact delete response 200 and 404 return true
     if customer.user.google_user.delete_contact(customer.google_contact_id)
-      customer.update_columns(deleted_at: Time.current, google_contact_id: nil)
+      if soft_delete
+        customer.update_columns(deleted_at: Time.current, google_contact_id: nil)
+      else
+        customer.destroy
+      end
     else
       errors.add(:customer, :delete_google_contact_failed)
     end
