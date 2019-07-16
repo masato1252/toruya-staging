7fcf8664e278907dec3640786d89b323e048d7ff
ilake <lake.ilakela+github@gmail.com>
2019-07-17 01:19:36 +0800

    bug, available_booking_options might be empty, no available booking options, so we should always check when there are options
    ref: https://github.com/ilake/kasaike/issues/537#issuecomment-511690332

--- app/webpacker/javascripts/components/management/settings/booking/booking_page_settings.js
+++ app/webpacker/javascripts/components/management/settings/booking/booking_page_settings.js
@@ -568,7 +568,7 @@ class BookingPageSettings extends React.Component {
       }
     }
 
-    if (options.length && available_booking_options.length) {
+    if (options.length) {
       const available_booking_option_ids = _.map(available_booking_options, (available_booking_option) => available_booking_option.id)
       const shop_name = this.props.shop_options.find((shop_option) => shop_option.value == shop_id).label
 
