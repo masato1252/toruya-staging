59951ed2388bb8ff259ae6ea5f78855b013fee4f
ilake <lake.ilakela+github@gmail.com>
2019-08-06 00:23:46 +0800

    When there is the same menu, the second staffs would merge into first menu, then second menu's staff would disapper, unlikely case
    https://rollbar.com/ilake/kasaike/items/758/occurrences/90634727219/

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -188,7 +188,8 @@ class ReservationsController < DashboardController
     @time_ranges = outcome.valid? ? outcome.result : nil
 
     @customer_max_load_capability = Array.wrap(reservation_params_hash[:menu_staffs_list]).map do |menu_staffs_list|
-      staff_ids = menu_staffs_list[:staff_ids].map { |hh| hh[:staff_id] }.compact
+      # XXX: When there is the same menu, the second staffs would merge into first menu, then second menu's staff would disapper, unlikely case
+      staff_ids = Array.wrap(menu_staffs_list[:staff_ids]).map { |hh| hh[:staff_id] }.compact
 
       if staff_ids.blank?
         0
