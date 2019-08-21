9d7dd0fe1304c8f4ed8aeca7e0d547e137de11ec
ilake <lake.ilakela+github@gmail.com>
2019-08-21 20:10:25 +0800

    Remove booking setting tour shop logo step
    ref: https://github.com/ilake/kasaike/issues/417#issuecomment-523343252

--- app/presenters/tours/booking_settings_presenter.rb
+++ app/presenters/tours/booking_settings_presenter.rb
@@ -1,6 +1,6 @@
 module Tours
   class BookingSettingsPresenter < BasePresenter
-    ALLOWED_ACCESS_CONTROLLERS = ["settings/shops", "settings/booking_options", "settings/booking_pages"].freeze
+    ALLOWED_ACCESS_CONTROLLERS = ["settings/booking_options", "settings/booking_pages"].freeze
 
     attr_reader :user, :h
 
@@ -14,14 +14,13 @@ module Tours
 
     def steps
       [
-        shop_logo_settings_step,
         booking_option_settings_step,
         booking_page_settings_step
       ]
     end
 
     def completed?
-      shop_logo_setup? && booking_option_setup? && booking_page_setup?
+      booking_option_setup? && booking_page_setup?
     end
 
     def booking_option_setup?
@@ -31,28 +30,6 @@ module Tours
     end
 
     private
-
-    def shop_logo_settings_step
-      Tours::Step.new({
-        done: shop_logo_setup?,
-        percentage: shop_logo_setup? ? "" : "0%",
-        title: I18n.t("settings.dashboard.booking_tour.shop_logo_setup"),
-        tasks: [
-          Tours::Task.new(
-            done: shop_logo_setup?,
-            title: I18n.t("settings.dashboard.booking_tour.shop_logo_setup"),
-            setting_path: h.settings_user_shops_path(user),
-            accessable_controller_in_tour: ALLOWED_ACCESS_CONTROLLERS.first(1)
-          )
-        ]
-      })
-    end
-
-    def shop_logo_setup?
-      return @shop_logo_setup if defined?(@shop_logo_setup)
-
-      @shop_logo_setup = user.shops.any? { |s| s.logo.attached? }
-    end
 
     def booking_option_settings_step
       Tours::Step.new({
@@ -64,8 +41,8 @@ module Tours
             done: booking_option_setup?,
             title: I18n.t("settings.dashboard.booking_tour.booking_option_setup"),
             setting_path: h.settings_user_booking_options_path(user),
-            setting_path_condition: shop_logo_setup? || booking_option_setup?,
-            accessable_controller_in_tour: ALLOWED_ACCESS_CONTROLLERS.first(2)
+            setting_path_condition: booking_option_setup?,
+            accessable_controller_in_tour: ALLOWED_ACCESS_CONTROLLERS.first(1)
           )
         ]
       })
@@ -81,8 +58,8 @@ module Tours
             done: booking_page_setup?,
             title: I18n.t("settings.dashboard.booking_tour.booking_page_setup"),
             setting_path: h.settings_user_booking_pages_path(user),
-            setting_path_condition: (shop_logo_setup? && booking_option_setup?) || booking_page_setup?,
-            accessable_controller_in_tour: ALLOWED_ACCESS_CONTROLLERS.first(3)
+            setting_path_condition: booking_option_setup? || booking_page_setup?,
+            accessable_controller_in_tour: ALLOWED_ACCESS_CONTROLLERS.first(2)
           )
         ]
       })
