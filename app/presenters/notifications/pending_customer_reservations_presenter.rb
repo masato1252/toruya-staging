a4115e520abd6d0d24d3694fe912643fe9560430
ilake <lake.ilakela+github@gmail.com>
2019-09-02 23:39:34 +0800

    Fix blue-bar flow needs fixes based on test
    ref: https://github.com/ilake/kasaike/issues/547#issuecomment-527043381
    https://github.com/ilake/kasaike/issues/554

--- app/presenters/notifications/pending_customer_reservations_presenter.rb
+++ app/presenters/notifications/pending_customer_reservations_presenter.rb
@@ -5,7 +5,7 @@ module Notifications
       oldest_reservation = oldest_customer_reservation&.reservation
 
       oldest_reservation ? [
-        "#{I18n.t("notifications.pending_customer_reservation_need_confirm", number: recent_pending_customer_reservations.count)} #{link_to(I18n.t("notifications.pending_customer_reservation_confirm"), h.user_customers_path(oldest_reservation.shop.user, customer_id: oldest_customer_reservation.customer_id, from_customer_id: oldest_customer_reservation.customer_id, reservation_id: oldest_reservation.id))}"
+        "#{I18n.t("notifications.pending_customer_reservation_need_confirm", number: recent_pending_customer_reservations.count)} #{link_to(I18n.t("notifications.pending_customer_reservation_confirm"), h.form_shop_reservations_path(oldest_reservation.shop, oldest_reservation.id))}"
       ] : []
     end
 
