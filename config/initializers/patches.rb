89ea3a723873e5c164da3ae13c51eb2e51dd6977
ilake <lake.ilakela+github@gmail.com>
2019-11-29 09:21:28 +0800

    What: It will show the warning: warning: already initialized constant Google::APIClient::ENV::OS_VERSION
    Why: It is annoying to see the warning
    How:  remove constant and assign again
    
    Move original patches to lib for future patches
    
    ref: https://github.com/ilake/kasaike/pull/577

--- config/initializers/patches.rb
+++ config/initializers/patches.rb
@@ -1,30 +1,5 @@
-# XXX: ruby after 2.5 don't allow don't support multiline headers, that break google-api-client https://github.com/googleapis/google-api-ruby-client/pull/648
-# But google_contacts_api only support google_api_client 0.8.6, so fix the header by this patch
-module Google
-  class APIClient
-    module ENV
-      OS_VERSION = begin
-        if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
-          # TODO(bobaman)
-          # Confirm that all of these Windows environments actually have access
-          # to the `ver` command.
-          `ver`.sub(/\s*\[Version\s*/, '/').sub(']', '').strip
-        elsif RUBY_PLATFORM =~ /darwin/i
-          "Mac OS X/#{`sw_vers -productVersion`}"
-        elsif RUBY_PLATFORM == 'java'
-          # Get the information from java system properties to avoid spawning a
-          # sub-process, which is not friendly in some contexts (web servers).
-          require 'java'
-          name = java.lang.System.getProperty('os.name')
-          version = java.lang.System.getProperty('os.version')
-          "#{name}/#{version}"
-        else
-          `uname -sr`.sub(' ', '/')
-        end
-      rescue Exception
-        RUBY_PLATFORM
-      end.strip
-    end
-  end
-end
+# Load on patches in lib/patches.
 
+Dir[Rails.root.join("lib", "patches", "**", "*.rb")].each do |patch_file|
+  require Pathname.new(patch_file)
+end
---
+++ lib/patches/google_api_client.rb
@@ -0,0 +1,32 @@
+# XXX: ruby after 2.5 don't allow don't support multiline headers, that break google-api-client https://github.com/googleapis/google-api-ruby-client/pull/648
+# But google_contacts_api only support google_api_client 0.8.6, so fix the header by this patch
+module Google
+  class APIClient
+    module ENV
+      remove_const(:OS_VERSION) if (defined?(OS_VERSION))
+
+      OS_VERSION = begin
+        if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
+          # TODO(bobaman)
+          # Confirm that all of these Windows environments actually have access
+          # to the `ver` command.
+          `ver`.sub(/\s*\[Version\s*/, '/').sub(']', '').strip
+        elsif RUBY_PLATFORM =~ /darwin/i
+          "Mac OS X/#{`sw_vers -productVersion`}"
+        elsif RUBY_PLATFORM == 'java'
+          # Get the information from java system properties to avoid spawning a
+          # sub-process, which is not friendly in some contexts (web servers).
+          require 'java'
+          name = java.lang.System.getProperty('os.name')
+          version = java.lang.System.getProperty('os.version')
+          "#{name}/#{version}"
+        else
+          `uname -sr`.sub(' ', '/')
+        end
+      rescue Exception
+        RUBY_PLATFORM
+      end.strip
+    end
+  end
+end
+
