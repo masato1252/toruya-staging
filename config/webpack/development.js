6353dc30841d6acd7f82cc2418d80ef8b369d92d
ilake <lake.ilakela+github@gmail.com>
2020-01-06 16:37:55 +0800

    Add BundleAnalyzerPlugin
    https://www.npmjs.com/package/webpack-bundle-analyzer

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+environment.plugins.append('BundleAnalyzer', new BundleAnalyzerPlugin())
+
 module.exports = environment.toWebpackConfig()
