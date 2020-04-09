684e670f5a836efa628a169321fdd99f60a2426c
ilake <lake.ilakela+github@gmail.com>
2020-04-09 14:54:22 +0800

    Tweak follow message

--- app/interactions/lines/follow_event.rb
+++ app/interactions/lines/follow_event.rb
@@ -5,11 +5,12 @@ class Lines::FollowEvent < ActiveInteraction::Base
   object :social_customer
 
   def execute
-    message = {
-      type: 'text',
-      text: "hello User #{event["source"]["userId"]}"
-    }
-
-    LineClient.reply(social_customer, event["replyToken"], message)
+    # message = {
+    #   type: 'text',
+    #   text: "hello User #{event["source"]["userId"]}"
+    # }
+    #
+    # LineClient.reply(social_customer, event["replyToken"], message)
+    Lines::MessageEvent.run(social_customer: social_customer, event: {})
   end
 end
