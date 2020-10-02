811b40e2307c046d108f6bf09310af61f609aeab
ilake <lake.ilakela+github@gmail.com>
2020-10-02 22:38:50 +0800

    [UserBot] Send line message when user sign in
    [What] Send line message when user sign in
    [Why] Notify users sign in successfully
    [How] Send the message when user's shop profile was completed,
     that means they already sign up, so it was a sign in behavior

--- app/interactions/identification_codes/verify_user.rb
+++ app/interactions/identification_codes/verify_user.rb
@@ -10,6 +10,12 @@ module IdentificationCodes
 
       if identification_code && (user = User.where(phone_number: phone_number).take)
         compose(SocialUsers::Connect, social_user: social_user, user: user, change_rich_menu: user.profile.address.present?)
+
+        # XXX: When user already filled in the company infomration, but verified code, again.
+        # This means it is a sign-in behavior
+        if user.profile.address.present?
+          Notifiers::LineUserSignedIn.run(receiver: social_user)
+        end
       end
 
       identification_code
