7fc39de790a09c58a2fca9f6d97f9b41a7dc3065
ilake <lake.ilakela+github@gmail.com>
2020-10-12 17:56:12 +0800

    [UserBot] redirect user to dashboard page after user sign in
    [What] redirect user to dashboard page after user sign in
    [Why] That will much more convenient
    [How] redirect it

--- app/webpacker/javascripts/components/user_bot/user_sign_up/index.js
+++ app/webpacker/javascripts/components/user_bot/user_sign_up/index.js
@@ -1,10 +1,33 @@
 "use strict";
 
-import React from "react";
+import React, { useEffect } from "react";
 import UserIdentificationFlow from "./user_identification_flow";
 import UserShopInfo from "./user_shop_info";
 import FlowController from "shared/flow_controller";
 
+const SignUpSuccessfulView = ({props}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      window.location = Routes.lines_user_bot_schedules_path()
+    }, 3000)
+  }, [])
+
+  return (
+    <div>
+      <h2 className="centerize">
+        {props.i18n.user_sign_up.page_title}
+      </h2>
+      <div className="whole-page-center final">
+        <div dangerouslySetInnerHTML={{ __html: props.i18n.user_sign_up.message.successful_message_html }} />
+        <br />
+        <i className="fa fa-spinner fa-spin fa-fw fa-2x"></i>
+        <div className="centerize">
+          Page would redirect to dashboard page automatically
+        </div>
+      </div>
+    </div>
+  )
+}
 export const UserSignUp = (props) => {
   return (
     <>
@@ -19,16 +42,7 @@ export const UserSignUp = (props) => {
         { (next, _) => <UserIdentificationFlow props={props} next={next} /> }
         { (_, prev) => <UserShopInfo
           props={props}
-          finalView={
-            <>
-              <h2 className="centerize">
-                {props.i18n.user_sign_up.page_title}
-              </h2>
-              <div className="whole-page-center final">
-                <div dangerouslySetInnerHTML={{ __html: props.i18n.user_sign_up.message.successful_message_html }} />
-              </div>
-            </>
-          }
+          finalView={<SignUpSuccessfulView props={props} />}
           />
         }
       </FlowController>
