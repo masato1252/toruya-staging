70686580cc378b12c3b129aaf21bd8ef130e9c68
ilake <lake.ilakela+github@gmail.com>
2020-12-19 08:19:52 +0800

    Remove cable
    [What] Remove cable connecting
    [Why] Since we don't support this feature for now
    [How] Removed it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount ActionCable.server => '/cable'
+  # mount ActionCable.server => '/cable'
   scope module: :lines, path: :lines, as: :lines do
     get "/identify_shop_customer/(:social_service_user_id)", action: "identify_shop_customer", as: :identify_shop_customer
     get "/contacts/social_service_user_id/:encrypted_social_service_user_id", action: "contacts", as: :contacts
