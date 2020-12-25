6e494571737dd6d2465d95292db2ccc1220c5e8f
ilake <lake.ilakela+github@gmail.com>
2020-12-25 19:45:41 +0800

    [UserBot] Switch to normal dashboard rich menu when users enter a empty unread messages page
    [What] Switch to normal dashboard rich menu when users enter a empty unread messages page
    [Why] Avoid the switch doesn't be switch to normal one correctly
    [How] Switch when unread messages  is empty

--- app/controllers/lines/user_bot/notifications_controller.rb
+++ app/controllers/lines/user_bot/notifications_controller.rb
@@ -1,5 +1,12 @@
 class Lines::UserBot::NotificationsController < Lines::UserBotDashboardController
   def index
     @messages = current_user.social_account.social_messages.includes(social_customer: :customer).unread
+
+    if @messages.empty?
+      UserBotLines::Actions::SwitchRichMenu.run(
+        social_user: social_user,
+        rich_menu_key: UserBotLines::RichMenus::Dashboard::KEY
+      )
+    end
   end
 end
