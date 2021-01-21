f02c445ac11cda357d0944cd1e6e45e2bcf09a35
ilake <lake.ilakela+github@gmail.com>
2021-01-21 13:32:11 +0800

    [omniauth-line] Fix omniauth-line bug, it doesn't support query_parameter
    [What] Fix omniauth-line bug, it doesn't support query_parameter
    [Why] Probably failed because the upgrade stuffs
    [How] fix it
    
    refs: https://github.com/zquestz/omniauth-google-oauth2/blob/da3fb724257f28feb208a3e00202b586fd410804/lib/omniauth/strategies/google_oauth2.rb#L32-L44

--- lib/patches/omniauth_line.rb
+++ lib/patches/omniauth_line.rb
@@ -4,6 +4,14 @@ module LineOAuth2
     # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
     options[:callback_url] || (full_host + script_name + callback_path)
   end
+
+  def authorize_params
+    super.tap do |params|
+      %i[prompt bot_prompt].each do |k|
+        params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+      end
+    end
+  end
 end
 
 OmniAuth::Strategies::Line.include(LineOAuth2)
