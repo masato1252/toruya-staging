679f66a4c1cde9cdda378fac7cb9c52e5c91c87a
ilake <lake.ilakela+github@gmail.com>
2021-02-18 22:48:12 +0800

    Tweak online service upsell page
    [What] Tweak online service upsell page
    [Why] We only want to support free online service for now, so upsell is more important page
    [How] Tweak it
    
    https://www.notion.so/New-online-service-upsell-page-429a1b3652ce44c2a3fc4ecae36ce626

--- app/webpacker/javascripts/components/user_bot/services/creation_flow/upsell_step.js
+++ app/webpacker/javascripts/components/user_bot/services/creation_flow/upsell_step.js
@@ -15,69 +15,32 @@ const UpsellStep = ({next, prev, step}) => {
       <ServiceFlowStepIndicator step={step} />
       <h3 className="header centerize break-line-content">{I18n.t("user_bot.dashboards.online_service_creation.what_want_to_upsell")}</h3>
       <div className="margin-around">
-        <label className="">
-          <input name="upsell" type="radio" value="no"
-            checked={upsell.type === "no"}
-            onChange={() => {
-              dispatch({
-                type: "SET_ATTRIBUTE",
-                payload: {
-                  attribute: "upsell",
-                  value: {
-                    type: "no",
-                  }
-                }
-              })
-            }}
-          />
-          {I18n.t("user_bot.dashboards.online_service_creation.no_upsell")}
-        </label>
-      </div>
-
-      <div>
         <label className="text-align-left">
-          <input name="upsell" type="radio" value="yes"
-            checked={upsell.type === "yes"}
-            onChange={() => {
-              dispatch({
-                type: "SET_ATTRIBUTE",
-                payload: {
-                  attribute: "upsell",
-                  value: {
-                    type: "yes",
-                  }
-                }
-              })
-            }}
-          />
-          {I18n.t("user_bot.dashboards.online_service_creation.want_upsell")}
-          {upsell.type === "yes" && (
-            <ReactSelect
-              placeholder={I18n.t("user_bot.dashboards.online_service_creation.select_upsell_product")}
-              value={ _.isEmpty(upsell.sale_page) ? "" : { label: upsell.sale_page.label }}
-              options={props.upsell_sales}
-              onChange={
-                (sale_page) => {
-                  dispatch({
-                    type: "SET_ATTRIBUTE",
-                    payload: {
-                      attribute: "upsell",
-                      value: {
-                        type: "yes",
-                        sale_page: sale_page.value
-                      }
+          <ReactSelect
+            placeholder={I18n.t("user_bot.dashboards.online_service_creation.select_upsell_product")}
+            value={ _.isEmpty(upsell.sale_page) ? "" : { label: upsell.sale_page.label }}
+            options={props.upsell_sales}
+            onChange={
+              (sale_page) => {
+                dispatch({
+                  type: "SET_ATTRIBUTE",
+                  payload: {
+                    attribute: "upsell",
+                    value: {
+                      type: "yes",
+                      sale_page: sale_page.value
                     }
-                  })
-                }
+                  }
+                })
               }
-            />
-          )}
+            }
+          />
         </label>
 
         {
           !_.isEmpty(upsell.sale_page) && (
             <>
-              {upsell.sale_page.start_time}
+              {upsell.sale_page.start_time}<br />
               {upsell.sale_page.end_time}
 
               <div className="sale-page margin-around">
@@ -98,6 +61,12 @@ const UpsellStep = ({next, prev, step}) => {
         <button onClick={next} className="btn btn-yellow"
           disabled={!upsell.type || (upsell.type === "yes" && _.isEmpty(upsell.sale_page))}>
           {I18n.t("action.next_step")}
+        </button>
+      </div>
+
+      <div className="action-block">
+        <button onClick={next} className="btn btn-tarco">
+          {I18n.t("action.skip_step")}
         </button>
       </div>
     </div>
