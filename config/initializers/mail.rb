cc68b5128ca39ba6ed4f6eb6c337b604380b6187
ilake <lake.ilakela+github@gmail.com>
2021-02-19 13:01:54 +0800

    Use sendgrid api v3 for user be upgraded to two-factor

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
 ActionMailer::Base.default_url_options[:host] = ENV['MAIL_DOMAIN']
 ActionMailer::Base.default_url_options[:protocol] = ENV['HTTP_PROTOCOL']
 ActionMailer::Base.default :from => ENV['MAIL_FROM']
-
-if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD'] && Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '465',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true,
-    :ssl => true
-  }
-  ActionMailer::Base.delivery_method = :smtp
-end
