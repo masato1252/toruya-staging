7688ebdf79329d2bfa2a0500cddf997e1c6f8a79
ilake <lake.ilakela+github@gmail.com>
2021-02-25 14:23:02 +0800

    New sale template title hint

--- lib/seeders/sale_template.rb
+++ lib/seeders/sale_template.rb
@@ -10,7 +10,7 @@ module Seeders
           { component: "input", type: "text", name: "problem", placeholder: "悩み", title: "ターゲットの悩みは何ですか？" },
           { component: "word", content: "を" },
           { component: "br" },
-          { component: "input", type: "text", name: "result", placeholder: "解決後の状態", title: "この予約メニューを利用することで\n悩みが解決されたターゲットは\nどんな未来を手に入れられますか？", font_size: "22px" },
+          { component: "input", type: "text", name: "result", placeholder: "解決後の状態", title: "この商品を利用することで\n悩みが解決されたターゲットは\nどんな未来を手に入れられますか？", font_size: "22px" },
           { component: "word", content: "にする" },
           { component: "br" },
           { component: "word", tag: "h4", name: "product_name", font_size: "24px" }
@@ -35,7 +35,7 @@ module Seeders
           { component: "input", name: "target", placeholder: "ターゲット", title: "この販売ページのターゲットは誰ですか？", type: "text" },
           { component: "word", content: "が" },
           { component: "br" },
-          { component: "input", type: "text", name: "result", placeholder: "解決後の状態", title: "この予約メニューを利用することで\n悩みが解決されたターゲットは\nどんな未来を手に入れられますか？", font_size: "22px" },
+          { component: "input", type: "text", name: "result", placeholder: "解決後の状態", title: "この商品を利用することで\n悩みが解決されたターゲットは\nどんな未来を手に入れられますか？", font_size: "22px" },
           { component: "word", content: "になる" },
           { component: "br" },
             { component: "word", tag: "h4", name: "product_name", font_size: "24px" }
