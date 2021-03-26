08e1c05eb8bfc369558aab18f3dedeefd9e69643
ilake <lake.ilakela+github@gmail.com>
2021-03-26 21:37:02 +0800

    fix notification

--- app/interactions/notifiers/reminders/reach_customers_limit.rb
+++ app/interactions/notifiers/reminders/reach_customers_limit.rb
@@ -8,7 +8,7 @@ module Notifiers
       def message
         if receiver.subscription.plan.free_level?
           I18n.t("notifier.reminders.free_plan_reach_customers_limit.message", user_name: receiver.name)
-        elsif receiver.subscription.plan.rank == Plan.max_legal_rank
+        elsif receiver.subscription.rank == Plan.max_legal_rank
           I18n.t("notifier.reminders.paid_plan_reach_whole_customers_limit.message", user_name: receiver.name)
         else
           I18n.t("notifier.reminders.paid_plan_reach_customers_limit.message", user_name: receiver.name, next_max_customer_limit: Plan.max_customers_limit(receiver.subscription.plan.level, receiver.subscription.rank + 1), user_last_name: receiver.profile.last_name || receiver.profile.phonetic_last_name)
