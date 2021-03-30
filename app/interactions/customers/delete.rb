4c4ccbbdf5656f40cb0711857b760af40194f2d8
ilake <lake.ilakela+github@gmail.com>
2021-03-30 09:37:00 +0800

    Fix customer delete behavior temporarily

--- app/interactions/customers/delete.rb
+++ app/interactions/customers/delete.rb
@@ -6,14 +6,14 @@ class Customers::Delete < ActiveInteraction::Base
 
   def execute
     # Google contact delete response 200 and 404 return true
-    if customer.user.google_user.delete_contact(customer.google_contact_id)
+    # if customer.user.google_user.delete_contact(customer.google_contact_id)
       if soft_delete
         customer.update_columns(deleted_at: Time.current, google_contact_id: nil)
       else
         customer.destroy
       end
-    else
-      errors.add(:customer, :delete_google_contact_failed)
-    end
+    # else
+    #   errors.add(:customer, :delete_google_contact_failed)
+    # end
   end
 end
