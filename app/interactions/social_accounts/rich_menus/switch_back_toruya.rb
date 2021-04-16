69802e2a5f9ac0c0a06539c9e75edb1d824b7d4b
ilake <lake.ilakela+github@gmail.com>
2021-04-16 20:49:47 +0800

    Put rich menu changes in a transaction to avoid only one success situation

--- app/interactions/social_accounts/rich_menus/switch_back_toruya.rb
+++ app/interactions/social_accounts/rich_menus/switch_back_toruya.rb
@@ -6,11 +6,13 @@ module SocialAccounts
       object :social_account
 
       def execute
-        if rich_menu = social_account.social_rich_menus.find_by(social_name: SocialRichMenu::LINE_OFFICIAL_RICH_MENU_KEY)
-          rich_menu.destroy
-        end
+        social_account.transaction do
+          if rich_menu = social_account.social_rich_menus.find_by(social_name: SocialRichMenu::LINE_OFFICIAL_RICH_MENU_KEY)
+            rich_menu.destroy
+          end
 
-        SocialAccounts::RichMenus::CustomerReservations.run(social_account: social_account)
+          SocialAccounts::RichMenus::CustomerReservations.run(social_account: social_account)
+        end
       end
     end
   end
