acdb0102668a9227ea7f0519d751b6da1e3437a0
ilake <lake.ilakela+github@gmail.com>
2021-04-19 17:45:12 +0800

    Check record existing

--- app/jobs/visit_improvement_job.rb
+++ app/jobs/visit_improvement_job.rb
@@ -8,18 +8,21 @@ class VisitImprovementJob < ApplicationJob
     when /booking_pages\/(\w+)/
       booking_page_slug = $1
 
-      booking_page = BookingPage.find_by(slug: booking_page_slug) || BookingPage.find_by(id: booking_page_slug)
-      ahoy_visit.update_columns(owner_id: booking_page.user_id, product_id: booking_page.id, product_type: "BookingPage")
+      if booking_page = BookingPage.find_by(slug: booking_page_slug) || BookingPage.find_by(id: booking_page_slug)
+        ahoy_visit.update_columns(owner_id: booking_page.user_id, product_id: booking_page.id, product_type: "BookingPage")
+      end
     when /sale_pages\/(\w+)/
       sale_page_slug = $1
 
-      sale_page = SalePage.find_by(slug: sale_page_slug)
-      ahoy_visit.update_columns(owner_id: sale_page.user_id, product_id: sale_page.id, product_type: "SalePage")
+      if sale_page = SalePage.find_by(slug: sale_page_slug)
+        ahoy_visit.update_columns(owner_id: sale_page.user_id, product_id: sale_page.id, product_type: "SalePage")
+      end
     when /online_services\/(\w+)/
       online_service_slug = $1
 
-      online_service = OnlineService.find_by(slug: online_service_slug)
-      ahoy_visit.update_columns(owner_id: online_service.user_id, product_id: online_service.id, product_type: "OnlineService")
+      if online_service = OnlineService.find_by(slug: online_service_slug)
+        ahoy_visit.update_columns(owner_id: online_service.user_id, product_id: online_service.id, product_type: "OnlineService")
+      end
     end
   end
 end
