7b771fbdaaa8f3ebd8d11f6b80e36d8ca5e63108
ilake <lake.ilakela+github@gmail.com>
2021-04-23 06:57:23 +0800

    Let test env read .env.test file

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -4,5 +4,5 @@ Rails.application.configure do
   config.x.env = ENV["PRODUCTION_ENV"] ? ActiveSupport::StringInquirer.new(ENV["PRODUCTION_ENV"]) : Rails.env
 end
 
-Rails.application.routes.default_url_options[:host] = ENV['MAIL_DOMAIN'] || "toruya.test"
-Rails.application.routes.default_url_options[:protocol] = ENV['HTTP_PROTOCOL'] || "https"
+Rails.application.routes.default_url_options[:host] = ENV['MAIL_DOMAIN']
+Rails.application.routes.default_url_options[:protocol] = ENV['HTTP_PROTOCOL']
