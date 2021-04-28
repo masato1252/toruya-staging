be93d7f28c6b87622f55c34aaba4f6537d53f8bd
ilake <lake.ilakela+github@gmail.com>
2021-04-28 17:01:47 +0800

    Fix channel name nil case
    Just in case the passed channel name is nil

--- app/jobs/hi_job.rb
+++ app/jobs/hi_job.rb
@@ -3,9 +3,9 @@
 class HiJob < ApplicationJob
   queue_as :low_priority
 
-  def perform(object, channel_name = "sayhi")
+  def perform(object, channel_name)
     if object.hi_message.present?
-      Slack::Web::Client.new.chat_postMessage(channel: channel_name, text: object.hi_message)
+      Slack::Web::Client.new.chat_postMessage(channel: channel_name || "sayhi", text: object.hi_message)
     end
   end
 end
