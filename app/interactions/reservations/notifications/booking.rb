90ef3ef17566a387f9d98046beb540b9148fd9d7
ilake <lake.ilakela+github@gmail.com>
2021-07-06 07:49:26 +0800

    Fix custom message for booking page
    It was forget to implement
    
    https://github.com/ilake/kasaike/pull/711

--- app/interactions/reservations/notifications/booking.rb
+++ app/interactions/reservations/notifications/booking.rb
@@ -34,13 +34,14 @@ module Reservations
       private
 
       def message
-        I18n.t(
-          "customer.notifications.sms.booking",
+        template = CustomMessage.template_of(booking_page, CustomMessage::BOOKING_PAGE_BOOKED)
+
+        Translator.perform(template, {
           customer_name: customer.name,
-          shop_name: shop.display_name,
-          shop_phone_number: shop.phone_number,
+          shop_name: booking_page.shop.display_name,
+          shop_phone_number: booking_page.shop.phone_number,
           booking_time: "#{I18n.l(reservation.start_time, format: :long_date_with_wday)} ~ #{I18n.l(reservation.end_time, format: :time_only)}"
-        )
+        })
       end
     end
   end
