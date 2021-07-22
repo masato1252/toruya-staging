0da3a856616aaf0da2540a158e282da13d80b0d7
ilake <lake.ilakela+github@gmail.com>
2021-07-22 08:46:54 +0800

    Fix typo

--- app/interactions/sales/online_services/purchase.rb
+++ app/interactions/sales/online_services/purchase.rb
@@ -31,7 +31,7 @@ module Sales
             relation.permission_state = :active
             relation.expire_at = product.current_expire_time
             relation.free_payment_state!
-            customer.write_attribute(:online_service_ids, (customer.read_attribute(:online_service_ids).concat([online_service.id.to_s])).uniq)
+            customer.write_attribute(:online_service_ids, (customer.read_attribute(:online_service_ids).concat([sale_page.product.id.to_s])).uniq)
             customer.touch
 
             Notifiers::OnlineServices::Purchased.perform_later(receiver: customer.social_customer, customer: customer, sale_page: relation.sale_page)
