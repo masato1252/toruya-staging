05eabfe145fb99ad7ecc0705fa1b1d84484a94ff
ilake <lake.ilakela+github@gmail.com>
2021-10-04 21:48:48 +0800

    Don't delay purchase message to avoid the message order is different
    [Why] https://www.notion.so/Purchase-notification-message-for-the-video-seminar-has-the-wrong-order-bca5cf8431c2447390a3d6404c4bb956
    [How] Don't delay it

--- app/interactions/online_services/attend.rb
+++ app/interactions/online_services/attend.rb
@@ -10,7 +10,7 @@ module OnlineServices
       customer.write_attribute(:online_service_ids, (customer.read_attribute(:online_service_ids).concat([online_service.id.to_s])).uniq)
       customer.save
 
-      Notifiers::OnlineServices::Purchased.perform_later(receiver: customer, sale_page: sale_page)
+      Notifiers::OnlineServices::Purchased.run(receiver: customer, sale_page: sale_page)
     end
   end
 end
