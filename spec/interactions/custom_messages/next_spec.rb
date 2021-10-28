2973bbe4a82df87b8f5b1d834578983b3a9ed9b3
ilake <lake.ilakela+github@gmail.com>
2021-10-28 22:01:58 +0800

    Add spec to make sure new custom message was scheduled as excpted

--- spec/interactions/custom_messages/next_spec.rb
+++ spec/interactions/custom_messages/next_spec.rb
@@ -129,6 +129,32 @@ RSpec.describe CustomMessages::Next do
 
         outcome
       end
+
+      context "when the new custom_message's after_days time was pass(before Time.current)" do
+        # The online_service start 4 days ago, but this new message was for after service started 3 days
+        let!(:relation) { FactoryBot.create(:online_service_customer_relation, :free, online_service: FactoryBot.create(:online_service, start_at: 4.days.ago)) }
+
+        it "does nothing" do
+          expect(Notifiers::CustomMessages::Send).not_to receive(:perform_at)
+
+          outcome
+        end
+      end
+
+      context "when the new custom_message's after_days time was not pass(before Time.current)" do
+        # The online_service start 2 days ago(customer join), and this new message was for after service started 3 days
+        let!(:relation) { FactoryBot.create(:online_service_customer_relation, :free, created_at: 2.days.ago) }
+
+        it "schedules the next custom message" do
+          expect(Notifiers::CustomMessages::Send).to receive(:perform_at).with({
+            schedule_at: service.start_at_for_customer(receiver).advance(days: current_custom_message.after_days).change(hour: 9),
+            custom_message: current_custom_message,
+            receiver: receiver
+          })
+
+          outcome
+        end
+      end
     end
 
     context "when it is from first time customer purchased(without custom_message params)" do
