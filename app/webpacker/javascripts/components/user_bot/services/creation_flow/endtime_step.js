356af30c7b23c9982c626819a3e053cec106e6a6
ilake <lake.ilakela+github@gmail.com>
2021-11-05 00:34:10 +0800

    End time step is the last step of course creation

--- app/webpacker/javascripts/components/user_bot/services/creation_flow/endtime_step.js
+++ app/webpacker/javascripts/components/user_bot/services/creation_flow/endtime_step.js
@@ -4,9 +4,10 @@ import React from "react";
 
 import { useGlobalContext } from "./context/global_state";
 import ServiceFlowStepIndicator from "./services_flow_step_indicator";
+import { SubmitButton } from "shared/components";
 
-const EndtimeStep = ({next, prev, step}) => {
-  const { props, dispatch, end_time } = useGlobalContext()
+const EndtimeStep = ({next, prev, step, lastStep}) => {
+  const { props, dispatch, end_time, createService, selected_solution } = useGlobalContext()
 
   return (
     <div className="form settings-flow centerize">
@@ -119,13 +120,26 @@ const EndtimeStep = ({next, prev, step}) => {
       </div>
 
       <div className="action-block">
-        <button onClick={next} className="btn btn-yellow" disabled={
-          !end_time.end_type ||
-            (end_time.end_type === "end_on_days" && !end_time.end_on_days) ||
-              (end_time.end_type === "end_at" && !end_time.end_time_date_part)
-          }>
-          {I18n.t("action.next_step")}
-        </button>
+        {selected_solution === 'course' ? (
+          <SubmitButton
+            disabled={
+              !end_time.end_type ||
+                (end_time.end_type === "end_on_days" && !end_time.end_on_days) ||
+                (end_time.end_type === "end_at" && !end_time.end_time_date_part)
+            }
+            handleSubmit={createService}
+            submitCallback={lastStep}
+            btnWord={I18n.t("user_bot.dashboards.online_service_creation.create_by_this_setting")}
+          />
+        ) : (
+          <button onClick={next} className="btn btn-yellow" disabled={
+            !end_time.end_type ||
+              (end_time.end_type === "end_on_days" && !end_time.end_on_days) ||
+                (end_time.end_type === "end_at" && !end_time.end_time_date_part)
+            }>
+            {I18n.t("action.next_step")}
+          </button>
+        )}
       </div>
     </div>
   )
