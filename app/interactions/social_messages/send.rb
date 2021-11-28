339c19ff2957a3d9ab9f2e9a94fa2c9ab193ffb8
ilake <lake.ilakela+github@gmail.com>
2021-11-28 08:12:01 +0800

    When customer social message send failed, send rollbar

--- app/interactions/social_messages/send.rb
+++ app/interactions/social_messages/send.rb
@@ -10,6 +10,13 @@ module SocialMessages
       if response&.code == "200" || Rails.env.test?
         social_message.update(sent_at: Time.current)
       else
+        Rollbar.error(
+          "Line send message failed",
+          user_id: social_message.social_customer.user_id,
+          social_message_id: social_message.id,
+          response: response.body
+        )
+
         raise ActiveRecord::Rollback
       end
     end
