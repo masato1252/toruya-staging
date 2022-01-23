51c0de86aa1bbe2ee24ec1b87573bcbea131a162
ilake <lake.ilakela+github@gmail.com>
2022-01-23 18:49:39 +0800

    Show correct error message 404
    [Why] The sale page should always be there, when customer got in here
    https://rollbar.com/ilake/kasaike/items/1255/occurrences/210428830257/

--- app/controllers/lines/customers/online_service_purchases_controller.rb
+++ app/controllers/lines/customers/online_service_purchases_controller.rb
@@ -26,7 +26,7 @@ class Lines::Customers::OnlineServicePurchasesController < Lines::CustomersContr
   private
 
   def sale_page
-    @sale_page = SalePage.find_by(slug: params[:slug])
+    @sale_page = SalePage.find_by!(slug: params[:slug])
   end
 
   def current_owner
