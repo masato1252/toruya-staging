18c93b1da4e16ebc3a299a653f68f526dfb2a0ae
ilake <lake.ilakela+github@gmail.com>
2022-03-11 07:54:16 +0800

    Delete a booking option relation while deleting booking option
    [Why] When a booking option was deleted, its relation with booking page is useless

--- app/models/booking_option.rb
+++ app/models/booking_option.rb
@@ -34,7 +34,7 @@ class BookingOption < ApplicationRecord
   has_many :menu_relations, class_name: "BookingOptionMenu"
   has_many :booking_option_menus, -> { order("booking_option_menus.priority") }
   has_many :menus, through: :booking_option_menus
-  has_many :booking_page_options
+  has_many :booking_page_options, dependent: :destroy
   has_many :booking_pages, through: :booking_page_options
 
   monetize :amount_cents
