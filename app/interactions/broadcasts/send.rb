777c453fa220d0b60094868b2896c5527a867807
ilake <lake.ilakela+github@gmail.com>
2022-05-22 08:03:30 +0800

    Fix broadcast notifiers name

--- app/interactions/broadcasts/send.rb
+++ app/interactions/broadcasts/send.rb
@@ -11,7 +11,7 @@ module Broadcasts
         customers = compose(Broadcasts::FilterCustomers, broadcast: broadcast)
 
         customers.find_each do |customer|
-          Notifiers::Broadcast.perform_later(receiver: customer, broadcast: broadcast)
+          Notifiers::Customers::Broadcast.perform_later(receiver: customer, broadcast: broadcast)
         end
 
         broadcast.update(state: :final, sent_at: Time.current, recipients_count: customers.size)
