3e13fd44386e0c981dd14305ddf272dc644180bd
ilake <lake.ilakela+github@gmail.com>
2022-10-15 11:25:42 +0800

    Fix picture unvariatnt issue
    https://rollbar.com/ilake/kasaike/items/1412/

--- app/serializers/sale_page_serializer.rb
+++ app/serializers/sale_page_serializer.rb
@@ -5,9 +5,25 @@ class SalePageSerializer
   attribute :id, :introduction_video_url, :flow, :end_time, :start_time, :sections_context, :solution_type, :normal_price
 
   attribute :content do |sale_page|
-    sale_page.content.merge(
-      picture_url: sale_page.picture.attached? ? Rails.application.routes.url_helpers.url_for(sale_page.picture.variant(combine_options: { resize: "750", flatten: true })) : nil
-    )
+    picture_url =
+      if sale_page.picture.attached?
+        begin
+          picture_variant = sale_page.picture.variant(combine_options: { resize: "750", flatten: true })
+          filename = picture_variant.blob.filename.to_s
+
+          if sale_page.picture.service.exist?(picture_variant.key)
+            Rails.application.routes.url_helpers.url_for(picture_variant)
+          else
+            Rails.application.routes.url_helpers.url_for(sale_page.picture)
+          end
+        rescue ActiveStorage::InvariableError
+          Rails.application.routes.url_helpers.url_for(sale_page.picture)
+        end
+      else
+        nil
+      end
+
+    sale_page.content.merge(picture_url: picture_url)
   end
 
   attribute :internal_name do |sale_page|
