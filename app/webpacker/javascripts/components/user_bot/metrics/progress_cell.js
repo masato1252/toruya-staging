5994db449a6a41e6603026afa0a9fcbfe4f887bf
ilake <lake.ilakela+github@gmail.com>
2022-12-13 20:36:26 +0800

    metric dashboard css improvement

--- app/webpacker/javascripts/components/user_bot/metrics/progress_cell.js
+++ app/webpacker/javascripts/components/user_bot/metrics/progress_cell.js
@@ -8,19 +8,17 @@ import I18n from 'i18n-js/index.js.erb';
 
 const ProgressCell = ({number}) => {
   return (
-    <div class="col-sm-3 centerize">
-      <div class="p-6 metric-cell">
-        <h4>{I18n.t("user_bot.dashboards.metrics.active_customers_rate")}</h4>
-        <div>
-          <p>{I18n.t("user_bot.dashboards.metrics.last_year")}</p>
-          <CircularProgressbarWithChildren className="progressbar" value={number * 100}>
-            <div>
-              <span className="text-6xl">{number * 100}</span>%
-            </div>
-          </CircularProgressbarWithChildren>
-        </div>
+    <>
+      <h4>{I18n.t("user_bot.dashboards.metrics.active_customers_rate")}</h4>
+      <div className="w-6-12 mx-auto">
+        <p>{I18n.t("user_bot.dashboards.metrics.last_year")}</p>
+        <CircularProgressbarWithChildren className="progressbar" value={number * 100} strokeWidth="13">
+          <div>
+            <span className="text-4xl">{number * 100}</span>%
+          </div>
+        </CircularProgressbarWithChildren>
       </div>
-    </div>
+    </>
   )
 }
 
