9c4de1a2fef057413086a98e6b675208ee40c136
ilake <lake.ilakela+github@gmail.com>
2022-12-15 15:34:50 +0800

    Only remind active user
    https://rollbar.com/ilake/kasaike/items/1463/occurrences/304467598311/

--- app/jobs/reservation_reminder_job.rb
+++ app/jobs/reservation_reminder_job.rb
@@ -4,8 +4,9 @@ class ReservationReminderJob < ApplicationJob
   queue_as :default
 
   def perform(reservation)
+    user = reservation.user
     reservation.customers.where(reminder_permission: true).each do |customer|
-      if reservation.remind_customer?(customer)
+      if user.subscription.active? && user.social_account.line_settings_finished? && reservation.remind_customer?(customer)
         Reservations::Notifications::Reminder.run!(customer: customer, reservation: reservation)
       end
     end
