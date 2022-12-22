949d8164f0f441814afd8d2f0eefd97480e9f406
ilake <lake.ilakela+github@gmail.com>
2022-12-22 23:38:26 +0800

    Fix db config access
    https://www.bigbinary.com/blog/rails-6-changed-activerecord-base-configurations-result-to-an-object

--- config/initializers/postgresql_database_tasks.rb
+++ config/initializers/postgresql_database_tasks.rb
@@ -5,7 +5,7 @@ module ActiveRecord
     class PostgreSQLDatabaseTasks
       def drop
         establish_master_connection
-        database = ActiveRecord::Base.connection.current_database
+        database = ActiveRecord::Base.configurations[Rails.env]['database']
         connection.select_all "select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname='#{database}' AND state='idle';"
         connection.drop_database database
       end
