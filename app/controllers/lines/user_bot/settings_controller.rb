d8f681a013ca0aee7b7c8da9ad9105c4c212c153
ilake <lake.ilakela+github@gmail.com>
2023-02-21 00:24:25 +0800

    get ready for web-admin page release
    https://www.notion.so/gardencities/get-ready-for-web-admin-page-release-ef7bccbe2b1d480d8889e596e2067897?pvs=4

--- app/controllers/lines/user_bot/settings_controller.rb
+++ app/controllers/lines/user_bot/settings_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Lines::UserBot::SettingsController < Lines::UserBotDashboardController
+  include ::MetricsHelpers
+
   def index
     @subscription = current_user.subscription
     @total_customer_count = current_user.customers.count
@@ -8,5 +10,6 @@ class Lines::UserBot::SettingsController < Lines::UserBotDashboardController
     @total_sale_page_count = current_user.sale_pages.count
     @total_sale_page_limit = Plan.max_sale_pages_limit(current_user.current_plan.level, @subscription.rank) || I18n.t("settings.dashboard.no_limit")
     @social_account = current_user.social_account
+    @customers_payment = CustomerPayment.completed.where(customer_id: current_user.customers.select(:id)).where("created_at > ?", metric_start_time).sum(:amount_cents).to_i
   end
 end
