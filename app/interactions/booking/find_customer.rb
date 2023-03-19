26e6ac0d64d68ed2443f0031bcfd9fd0e4d3d8f3
ilake <lake.ilakela+github@gmail.com>
2023-03-19 17:05:32 +0800

    Send duplicate message to slack

--- app/interactions/booking/find_customer.rb
+++ app/interactions/booking/find_customer.rb
@@ -17,12 +17,7 @@ module Booking
       )
 
       if customers_hash[:matched_customers].length > 1
-        NotificationMailer.duplicate_customers(
-          booking_page,
-          customers_hash[:matched_customers],
-          customers_hash[:found_customer],
-          phone_number
-        ).deliver_later
+        SlackClient.send(channel: 'toruya_users_support', text: "user_id: #{booking_page.user_id}, had duplicate_customers #{customers_hash[:matched_customers].map(&:id)}, had same name #{customers_hash[:found_customer].name} and phone_number #{phone_number}")
       end
 
       customers_hash[:found_customer]
