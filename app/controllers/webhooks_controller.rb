6f9de8fb4f6e2fbcc7927833ef581330c986d18c
ilake <lake.ilakela+github@gmail.com>
2023-05-23 19:13:08 +0800

    Fix ActionController::InvalidAuthenticityToken
    https://app.rollbar.com/a/ilake/fix/item/kasaike/1582?utm_campaign=exp_repeat_item_message&utm_medium=slack&utm_source=rollbar-notification&showOccurrences=true#detail

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class WebhooksController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   skip_before_action :track_ahoy_visit
   abstract!
 end
