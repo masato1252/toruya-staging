8fbb87cfaa494af64733b75278a75e50f6e79ae5
ilake <lake.ilakela+github@gmail.com>
2023-06-03 20:47:11 +0800

    Fix db import script due to rails upgrade

--- config/initializers/postgresql_database_tasks.rb
+++ config/initializers/postgresql_database_tasks.rb
@@ -5,7 +5,7 @@ module ActiveRecord
     class PostgreSQLDatabaseTasks
       def drop
         establish_master_connection
-        database = ActiveRecord::Base.configurations[Rails.env]['database']
+        database = ActiveRecord::Base.configurations.configurations.find {|env| env.env_name == Rails.env }
         connection.select_all "select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname='#{database}' AND state='idle';"
         connection.drop_database database
       end
