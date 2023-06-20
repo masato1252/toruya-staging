fb1c68ccba791ae7a53cc8d2038fc66752da7337
ilake <lake.ilakela+github@gmail.com>
2023-06-20 17:08:25 +0800

    Show booking page metric
    https://www.notion.so/gardencities/show-conversion-data-for-booking-sales-pages-too-a83659d47af74a289d7b834b79bc2a47?pvs=4

--- app/interactions/metrics/sale_pages_conversions.rb
+++ app/interactions/metrics/sale_pages_conversions.rb
@@ -47,21 +47,22 @@ module Metrics
         visit_count = visit_scope.where(product_id: product_id, product_type: "SalePage").where(started_at: metric_period).count
         relations = OnlineServiceCustomerRelation.where(created_at: metric_period, sale_page_id: product_id, bundled_service_id: nil).current
         relations = relations.where(online_service_id: online_service_id) if online_service_id
+        reservation_customer_relations = ReservationCustomer.where(created_at: metric_period, booking_page_id: sale_page.product_id) if sale_page&.is_booking_page?
 
-        purchased_count =
+        purchased_count, total_revenue =
           if sale_page&.is_booking_page?
-            nil
+            [reservation_customer_relations.count, CustomerPayment.where(product_type: "ReservationCustomer", product_id: reservation_customer_relations.select(:id)).completed.sum(:amount_cents).to_i]
           else
-            relations.count
+            [relations.count, CustomerPayment.where(product_type: "OnlineServiceCustomerRelation", product_id: relations.select(:id)).completed.sum(:amount_cents).to_i]
           end
 
         {
           label: sale_page&.internal_sale_name,
           visit_count: visit_count,
           purchased_count: purchased_count,
-          rate: purchased_count ? purchased_count / [visit_count, 1].max.to_f : nil,
-          total_revenue: purchased_count ? helpers.number_with_delimiter(CustomerPayment.where(product_type: "OnlineServiceCustomerRelation", product_id: relations.select(:id)).completed.sum(:amount_cents).to_i) : nil,
-          format_rate: purchased_count ? helpers.number_to_percentage(purchased_count * 100 / [visit_count, 1].max.to_f, precision: 1) : nil
+          rate: purchased_count / [visit_count, 1].max.to_f,
+          total_revenue: helpers.number_with_delimiter(total_revenue),
+          format_rate: helpers.number_to_percentage(purchased_count * 100 / [visit_count, 1].max.to_f, precision: 1)
         }
       end
 
