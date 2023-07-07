e4e5dfafa1c41582fe5ee9723d9b4e937f655474
ilake <lake.ilakela+github@gmail.com>
2023-07-08 06:47:53 +0800

    Tweak admin scenario pages

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -4,11 +4,11 @@ module AdminHelper
   def scenario_label(scenario_key)
     case scenario_key
     when CustomMessages::Users::Template::BOOKING_PAGE_NOT_ENOUGH_PAGE_VIEW
-      "[Booking#1] #{scenario_key}"
+      "[Booking#1] #{scenario_key.titleize}"
     when CustomMessages::Users::Template::BOOKING_PAGE_NOT_ENOUGH_BOOKING
-      "[Booking#2] #{scenario_key}"
+      "[Booking#2] #{scenario_key.titleize}"
     else
-      scenario_key
+      scenario_key.titleize
     end
   end
 end
