54d429c22ca6d75327171eb0aa122c5bd2c0177c
ilake <lake.ilakela+github@gmail.com>
2023-08-18 16:17:48 +0800

    Ai build need to take a long time, use different priority queue

--- app/jobs/ai_build_job.rb
+++ app/jobs/ai_build_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AiBuildJob < ApplicationJob
-  queue_as :default
+  queue_as :low_priority
 
   def perform(user_id, url)
     AI_BUILD.perform(user_id, url)
--- config/initializers/delayer.rb
+++ config/initializers/delayer.rb
@@ -5,3 +5,9 @@ require "retry"
 
 ActiveInteraction::Base.extend(ActiveInteractionDelayer)
 ActiveInteraction::Base.include(Retry)
+
+Delayed::Worker.queue_attributes = {
+  high_priority: { priority: 1 },
+  default: { priority: 10 },
+  low_priority: { priority: 20 },
+}
