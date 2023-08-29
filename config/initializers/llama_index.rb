1ac6056b2ad12d81c3bcb002cacb2d79f6700798
ilake <lake.ilakela+github@gmail.com>
2023-08-29 17:16:11 +0800

    for ci testing

--- config/initializers/llama_index.rb
+++ config/initializers/llama_index.rb
@@ -2,11 +2,13 @@ require 'pycall'
 require 'pycall/import'
 include PyCall::Import
 
-pyimport :sys
-lib_directory = Rails.root.join('lib', 'llama_index')
-sys.path.append(lib_directory.to_s)
-llama_index_pinecone_build = PyCall.import_module("pinecone_build")
-llama_index_pinecone_query = PyCall.import_module("pinecone_query")
+if !Rails.env.test?
+  pyimport :sys
+  lib_directory = Rails.root.join('lib', 'llama_index')
+  sys.path.append(lib_directory.to_s)
+  llama_index_pinecone_build = PyCall.import_module("pinecone_build")
+  llama_index_pinecone_query = PyCall.import_module("pinecone_query")
 
-AI_BUILD = llama_index_pinecone_build.LlamaIndexPineconeBuild
-AI_QUERY = llama_index_pinecone_query.LlamaIndexPineconeQuery
+  AI_BUILD = llama_index_pinecone_build.LlamaIndexPineconeBuild
+  AI_QUERY = llama_index_pinecone_query.LlamaIndexPineconeQuery
+end
