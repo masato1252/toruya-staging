ca571c677ea337492c30bb9dd8a5aa8f5b79722a
ilake <lake.ilakela+github@gmail.com>
2023-10-30 06:59:02 +0800

    Clean account settings only after switch to official account

--- app/interactions/social_accounts/clean.rb
+++ app/interactions/social_accounts/clean.rb
@@ -7,16 +7,19 @@ module SocialAccounts
     def execute
       account = user.social_accounts.first
       user.social_customers.where(is_owner: true).update_all(is_owner: false, customer_id: nil)
-      SocialAccounts::RichMenus::SwitchToOfficial.run(social_account: account)
-      account&.update(
-        channel_secret: nil,
-        channel_token: nil,
-        label: nil,
-        login_channel_secret: nil,
-        basic_id: nil,
-        channel_id: nil,
-        login_channel_id: nil
-      )
+      outcome = SocialAccounts::RichMenus::SwitchToOfficial.run(social_account: account)
+
+      if outcome.valid?
+        account&.update(
+          channel_secret: nil,
+          channel_token: nil,
+          label: nil,
+          login_channel_secret: nil,
+          basic_id: nil,
+          channel_id: nil,
+          login_channel_id: nil
+        )
+      end
 
       account
     end
