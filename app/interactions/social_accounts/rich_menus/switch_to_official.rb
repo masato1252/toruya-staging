cbe4cde87140e3de8d62fa8b3c848b1e458137bd
ilake <lake.ilakela+github@gmail.com>
2023-11-29 09:28:18 +0800

    Avoid some users don't finished line setting, no rich menus

--- app/interactions/social_accounts/rich_menus/switch_to_official.rb
+++ app/interactions/social_accounts/rich_menus/switch_to_official.rb
@@ -6,14 +6,17 @@ module SocialAccounts
       object :social_account
 
       def execute
-        return if social_account.current_rich_menu.official?
+        return if social_account.current_rich_menu&.official?
 
         social_account.transaction do
           social_account.social_rich_menus.default.find_each do |rich_menu|
             compose(::RichMenus::Delete, social_rich_menu: rich_menu)
           end
-          social_account.current_rich_menu.update(current: nil)
-          ::RichMenus::Unlink.perform_later(social_rich_menu: social_account.current_rich_menu)
+
+          if social_account.current_rich_menu
+            social_account.current_rich_menu.update(current: nil)
+            ::RichMenus::Unlink.perform_later(social_rich_menu: social_account.current_rich_menu)
+          end
 
           new_current_rich_menu = social_account.social_rich_menus.find_or_create_by(social_name: SocialRichMenu::LINE_OFFICIAL_RICH_MENU_KEY)
           new_current_rich_menu.update(current: true)
