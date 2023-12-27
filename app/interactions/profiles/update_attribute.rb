a8b1876a39154e2ccb5facf688fba16fa6918dd2
ilake <lake.ilakela+github@gmail.com>
2023-12-27 16:06:41 +0800

    Avoid no photo changes update
    https://app.rollbar.com/a/ilake/fix/item/kasaike/1773

--- app/interactions/profiles/update_attribute.rb
+++ app/interactions/profiles/update_attribute.rb
@@ -51,10 +51,13 @@ module Profiles
           end
         when "logo"
           logo_params = attrs[:logo]
-          if logo_params.content_type.in?(Shops::Update::CONTENT_TYPES) && logo_params.size.between?(0, 0.05.megabyte)
-            profile.logo.attach(logo_params)
-          else
-            errors.add(:profile, :logo_invalid)
+
+          if logo_params
+            if logo_params.content_type.in?(Shops::Update::CONTENT_TYPES) && logo_params.size.between?(0, 0.05.megabyte)
+              profile.logo.attach(logo_params)
+            else
+              errors.add(:profile, :logo_invalid)
+            end
           end
         end
 
