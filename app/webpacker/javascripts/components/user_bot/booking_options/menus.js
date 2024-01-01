1c1d046fe84af6d4a406e13ea2c65453f37e74da
ilake <lake.ilakela+github@gmail.com>
2024-01-01 19:03:02 +0800

    Revert option menu link
    [WHY] it make users confuse clicking

--- app/webpacker/javascripts/components/user_bot/booking_options/menus.js
+++ app/webpacker/javascripts/components/user_bot/booking_options/menus.js
@@ -16,7 +16,7 @@ const SortableMenuOption = sortableElement(({props, booking_option_id, menu, i18
         <div className="menu-info">
           <DragHandle />
           <div>
-            <a href={Routes.lines_user_bot_settings_menu_path(props.business_owner_id, menu.value)}><h3>{menu.label}</h3></a>
+            <h3>{menu.label}</h3>
             <div className="desc">{i18n.required_time}{menu.required_time}{i18n.minute}</div>
           </div>
         </div>
