5a8857721cb0eaf66bab29c8d27812ac5263ccca
ilake <lake.ilakela+github@gmail.com>
2024-02-21 16:13:29 +0800

    Avoid unexpected hash
    Use base64 urlsafe_encode64 to make sure the parameter is url safe

--- lib/message_encryptor.rb
+++ lib/message_encryptor.rb
@@ -2,14 +2,17 @@
 
 module MessageEncryptor
   def self.encrypt(message, expires_at: nil)
-    crypt.encrypt_and_sign(message, expires_at: expires_at)
+    Base64.urlsafe_encode64(crypt.encrypt_and_sign(message, expires_at:))
   end
 
   def self.decrypt(encrypted_content)
+    decode64_hash = Base64.urlsafe_decode64(encrypted_content)
+    crypt.decrypt_and_verify(decode64_hash)
+  rescue StandardError
     crypt.decrypt_and_verify(encrypted_content)
   end
 
   def self.crypt
-    ActiveSupport::MessageEncryptor.new(Rails.application.secrets.message_encryptor_key[0..31], cipher: "aes-256-cbc")
+    ActiveSupport::MessageEncryptor.new(Rails.application.secrets.message_encryptor_key[0..31], cipher: 'aes-256-cbc')
   end
 end
