261a49233d37d27611ddfe64a89555ef422774ad
ilake <lake.ilakela+github@gmail.com>
2024-02-29 09:27:34 +0800

    Fix rollbar
    https://app.rollbar.com/a/ilake/fix/item/kasaike/1863?utm_campaign=new_item_message&utm_medium=slack&utm_source=rollbar-notification

--- app/webpacker/javascripts/controllers/change_log_notification_controller.js
+++ app/webpacker/javascripts/controllers/change_log_notification_controller.js
@@ -2,9 +2,9 @@ import "whatwg-fetch";
 import { Controller } from "stimulus"
 
 export default class ChangeLogNotificationController extends Controller {
-  static version = 2.0.1
+  static version = "2.0.1";
   static targets = [
-    "content",
+    "content"
   ];
 
   connect() {
