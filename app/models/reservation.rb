5c929a7dfde24aacf7ec10a57a2fd9f5e93d8b45
ilake <lake.ilakela+github@gmail.com>
2024-03-04 07:51:58 +0800

    Fix reservation customers broadcast
    https://app.rollbar.com/a/ilake/fix/item/kasaike/1873?utm_campaign=new_item_message&utm_medium=slack&utm_source=rollbar-notification
    In broadcast new demo page, no real reservation customer

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -139,7 +139,7 @@ class Reservation < ApplicationRecord
       end_time: end_time,
       meeting_url: meeting_url,
       product_name: reservation_customer&.booking_option&.display_name.presence || menus_sentence,
-      booking_page_url: reservation_customer.booking_page ? Rails.application.routes.url_helpers.booking_page_url(reservation_customer.booking_page.slug, last_booking_option_id: reservation_customer.booking_option_id) : ""
+      booking_page_url: reservation_customer&.booking_page ? Rails.application.routes.url_helpers.booking_page_url(reservation_customer.booking_page.slug, last_booking_option_id: reservation_customer.booking_option_id) : ""
     )
   end
 
