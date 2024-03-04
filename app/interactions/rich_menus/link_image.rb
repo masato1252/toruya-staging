d1c644ded45278811833e0c59b96e9ae6d46f313
ilake <lake.ilakela+github@gmail.com>
2024-03-04 13:25:42 +0800

    Add rollbar for invalid image

--- app/interactions/rich_menus/link_image.rb
+++ app/interactions/rich_menus/link_image.rb
@@ -8,11 +8,22 @@ module RichMenus
     object :social_rich_menu
 
     def execute
-      ::LineClient.create_rich_menu_image(
+      response = ::LineClient.create_rich_menu_image(
         social_account: social_account,
         rich_menu_id: social_rich_menu.social_rich_menu_id,
         file_path: file_url_or_path
       )
+
+      unless response.is_a?(Net::HTTPOK)
+        if social_rich_menu.image.attached?
+          base64_image = ActiveSupport::Base64.encode64(URI.open(file_url_or_path) { |io| io.read })
+          Rollbar.error("Invalid Rich Menu image", response: response.body, url: file_url_or_path, base64_image: base64_image)
+        else
+          Rollbar.error("Invalid Rich Menu image", response: response.body, url: file_url_or_path)
+        end
+      end
+
+      response
     end
 
     private
