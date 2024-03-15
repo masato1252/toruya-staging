bef1ff17bed542cd542b55bc6483283e873390a1
ilake <lake.ilakela+github@gmail.com>
2024-03-15 08:14:27 +0800

    Release 2.0.3 https://toruya.com/updates/ ver 2.0.3 in 2024-03-15

--- app/webpacker/javascripts/controllers/change_log_notification_controller.js
+++ app/webpacker/javascripts/controllers/change_log_notification_controller.js
@@ -2,7 +2,7 @@ import "whatwg-fetch";
 import { Controller } from "stimulus"
 
 export default class ChangeLogNotificationController extends Controller {
-  static version = "2.0.2";
+  static version = "2.0.3";
   static targets = [
     "content"
   ];
