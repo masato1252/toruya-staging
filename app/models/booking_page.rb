9cebc5bc64eac80f7811349f22bcfd4feb72e242
ilake <lake.ilakela+github@gmail.com>
2024-05-26 14:43:08 +0800

    Avoid no provider

--- app/models/booking_page.rb
+++ app/models/booking_page.rb
@@ -120,12 +120,20 @@ class BookingPage < ApplicationRecord
   def payment_solution
     case payment_provider
     when AccessProvider.providers[:stripe_connect]
-      { solution: user.stripe_provider.provider, stripe_key: user.stripe_provider.publishable_key }
+      if user.stripe_provider
+        { solution: user.stripe_provider.provider, stripe_key: user.stripe_provider.publishable_key }
+      else
+        {}
+      end
     when AccessProvider.providers[:square]
-      result = user.square_client.locations.list_locations
-      square_location_id = result.data[:locations].last[:id]
+      if user.square_client
+        result = user.square_client.locations.list_locations
+        square_location_id = result.data[:locations].last[:id]
 
-      { solution: user.square_provider.provider, square_app_id: Rails.application.secrets.square_app_id, square_location_id: square_location_id }
+        { solution: user.square_provider.provider, square_app_id: Rails.application.secrets.square_app_id, square_location_id: square_location_id }
+      else
+        {}
+      end
     else
       {}
     end
