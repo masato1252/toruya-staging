689da4ed1fe33376f7827722fc4489eddaf9ec02
ilake <lake.ilakela+github@gmail.com>
2024-06-21 09:26:52 +0800

    Add more logs

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -20,70 +20,44 @@ class AdminController < ApplicationController
     user = User.find_by(id: params[:user_id])
     social_account = user.social_account
     booking_pages = BookingPage.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |booking_page|
-      {
-        label: "Booking Page (#{ApplicationController.helpers.link_to(booking_page.id, Rails.application.routes.url_helpers.booking_page_url(booking_page))}) created".html_safe,
-        time: booking_page.created_at
-      }
+      { label: "Booking Page (#{ApplicationController.helpers.link_to(booking_page.id, Rails.application.routes.url_helpers.booking_page_url(booking_page))}) created".html_safe, time: booking_page.created_at }
     end
     sale_pages = SalePage.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |sale_page|
-      {
-        label: "Sale page (#{ApplicationController.helpers.link_to(sale_page.id, Rails.application.routes.url_helpers.sale_page_url(sale_page.slug))}) created".html_safe,
-        time: sale_page.created_at
-      }
+      { label: "Sale page (#{ApplicationController.helpers.link_to(sale_page.id, Rails.application.routes.url_helpers.sale_page_url(sale_page.slug))}) created".html_safe, time: sale_page.created_at }
     end
-    reservations = Reservation.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |reservation|
-      {
-        label: "Reservation (#{reservation.id}) created",
-        time: reservation.created_at
-      }
+    reservation_ids = Reservation.where(user_id: user.id).where("created_at > ?", 1.months.ago).pluck(:id)
+    manual_reservations = ReservationCustomer.where(reservation_id: reservation_ids, booking_page_id: nil).map do |reservation_customer|
+      { label: "Manual Reservation created", time: reservation_customer.created_at }
+    end
+    booking_page_reservations = ReservationCustomer.where(reservation_id: reservation_ids).where.not(booking_page_id: nil).map do |reservation_customer|
+      { label: "Booking page Reservation created", time: reservation_customer.created_at }
+    end
+    line_customers = SocialCustomer.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |customer|
+      { label: "Line Customer created", time: customer.created_at }
     end
     customers = Customer.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |customer|
-      {
-        label: "Customer (#{customer.id}) created",
-        time: customer.created_at
-      }
+      { label: "Customer (#{customer.id}) created", time: customer.created_at }
     end
     services = OnlineService.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |service|
-      {
-        label: "OnlineService created",
-        time: service.created_at
-      }
+      { label: "OnlineService created", time: service.created_at }
     end
     broadcasts = Broadcast.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |broadcast|
-      {
-        label: "Broadcast created",
-        time: broadcast.created_at
-      }
+      { label: "Broadcast created", time: broadcast.created_at }
     end
     booking_options = BookingOption.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |booking_option|
-      {
-        label: "Booking Price created",
-        time: booking_option.created_at
-      }
+      { label: "Booking Price created", time: booking_option.created_at }
     end
     personal_schedules = CustomSchedule.closed.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |custom_schedule|
-      {
-        label: "Personal Schedule created",
-        time: custom_schedule.created_at
-      }
+      { label: "Personal Schedule created", time: custom_schedule.created_at }
     end
     menus = Menu.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |menu|
-      {
-        label: "Menu created",
-        time: menu.created_at
-      }
+      { label: "Menu created", time: menu.created_at }
     end
     customer_messages = SocialMessage.where(social_account: social_account).where(message_type: [:customer]).where("created_at > ?", 1.months.ago).map do |message|
-      {
-        label: "Customer Message created",
-        time: message.created_at
-      }
+      { label: "Customer Message created", time: message.created_at }
     end
     user_messages = SocialMessage.where(social_account: social_account).where(message_type: [:staff]).where("created_at > ?", 1.months.ago).map do |message|
-      {
-        label: "User Message created",
-        time: message.created_at
-      }
+      { label: "User Message created", time: message.created_at }
     end
 
     @logs = [
@@ -91,7 +65,9 @@ class AdminController < ApplicationController
       customer_messages,
       user_messages,
       sale_pages,
-      reservations,
+      manual_reservations,
+      booking_page_reservations,
+      line_customers,
       customers,
       services,
       broadcasts,
