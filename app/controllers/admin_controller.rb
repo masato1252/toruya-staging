8b94a44af9fd666d45a3bbc34e13ae5f96b49018
ilake <lake.ilakela+github@gmail.com>
2024-06-21 13:57:56 +0800

    translations for logs

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -20,44 +20,44 @@ class AdminController < ApplicationController
     user = User.find_by(id: params[:user_id])
     social_account = user.social_account
     booking_pages = BookingPage.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |booking_page|
-      { label: "Booking Page (#{ApplicationController.helpers.link_to(booking_page.id, Rails.application.routes.url_helpers.booking_page_url(booking_page))}) created".html_safe, time: booking_page.created_at }
+      { label: "予約ページ作成 (#{ApplicationController.helpers.link_to(booking_page.id, Rails.application.routes.url_helpers.booking_page_url(booking_page))})".html_safe, time: booking_page.created_at }
     end
     sale_pages = SalePage.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |sale_page|
-      { label: "Sale page (#{ApplicationController.helpers.link_to(sale_page.id, Rails.application.routes.url_helpers.sale_page_url(sale_page.slug))}) created".html_safe, time: sale_page.created_at }
+      { label: "宣伝用LP作成 (#{ApplicationController.helpers.link_to(sale_page.id, Rails.application.routes.url_helpers.sale_page_url(sale_page.slug))})".html_safe, time: sale_page.created_at }
     end
     reservation_ids = Reservation.where(user_id: user.id).where("created_at > ?", 1.months.ago).pluck(:id)
     manual_reservations = ReservationCustomer.where(reservation_id: reservation_ids, booking_page_id: nil).map do |reservation_customer|
-      { label: "Manual Reservation created", time: reservation_customer.created_at }
+      { label: "予約作成", time: reservation_customer.created_at }
     end
     booking_page_reservations = ReservationCustomer.where(reservation_id: reservation_ids).where.not(booking_page_id: nil).map do |reservation_customer|
-      { label: "Booking page Reservation created", time: reservation_customer.created_at }
+      { label: "予約ページ予約作成", time: reservation_customer.created_at }
     end
     line_customers = SocialCustomer.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |customer|
-      { label: "Line Customer created", time: customer.created_at }
+      { label: "Line 顧客作成", time: customer.created_at }
     end
     customers = Customer.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |customer|
-      { label: "Customer (#{customer.id}) created", time: customer.created_at }
+      { label: "顧客作成(#{customer.id})", time: customer.created_at }
     end
     services = OnlineService.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |service|
-      { label: "OnlineService created", time: service.created_at }
+      { label: "サービス作成", time: service.created_at }
     end
     broadcasts = Broadcast.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |broadcast|
-      { label: "Broadcast created", time: broadcast.created_at }
+      { label: "セグメント配信作成", time: broadcast.created_at }
     end
     booking_options = BookingOption.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |booking_option|
-      { label: "Booking Price created", time: booking_option.created_at }
+      { label: "予約価格作成", time: booking_option.created_at }
     end
     personal_schedules = CustomSchedule.closed.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |custom_schedule|
-      { label: "Personal Schedule created", time: custom_schedule.created_at }
+      { label: "個人の予定作成", time: custom_schedule.created_at }
     end
     menus = Menu.where(user_id: user.id).where("created_at > ?", 1.months.ago).map do |menu|
-      { label: "Menu created", time: menu.created_at }
+      { label: "メニュー作成", time: menu.created_at }
     end
     customer_messages = SocialMessage.where(social_account: social_account).where(message_type: [:customer]).where("created_at > ?", 1.months.ago).map do |message|
-      { label: "Customer Message created", time: message.created_at }
+      { label: "メッセージ受信", time: message.created_at }
     end
     user_messages = SocialMessage.where(social_account: social_account).where(message_type: [:staff]).where("created_at > ?", 1.months.ago).map do |message|
-      { label: "User Message created", time: message.created_at }
+      { label: "メッセージ送信", time: message.created_at }
     end
 
     @logs = [
