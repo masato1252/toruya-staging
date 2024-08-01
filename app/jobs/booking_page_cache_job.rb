440edb85e805aad11a672a8d750eb9cf035d93e4
ilake <lake.ilakela+github@gmail.com>
2024-08-02 07:37:47 +0800

    Make cache lower priority job

--- app/jobs/booking_page_cache_job.rb
+++ app/jobs/booking_page_cache_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BookingPageCacheJob < ApplicationJob
-  queue_as :default
+  queue_as :low_priority
 
   def perform(booking_page)
     (Date.today..Date.today.next_month.next_month).each do |date|
