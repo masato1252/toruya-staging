9e8d6f865ba2a9bc39b9473f4958a2ebb991c92a
ilake <lake.ilakela+github@gmail.com>
2024-08-08 18:34:07 +0800

    Do the cache every 2 hours

--- app/jobs/booking_page_cache_job.rb
+++ app/jobs/booking_page_cache_job.rb
@@ -4,8 +4,11 @@ class BookingPageCacheJob < ApplicationJob
   queue_as :low_priority
 
   def perform(booking_page)
-    (Date.today..Date.today.next_month.next_month).each do |date|
-      ::Booking::Cache.run(booking_page: booking_page, date: date)
+    # Every 2 hours
+    if Time.current.hour % 2 == 0
+      (Date.today..Date.today.next_month.next_month).each do |date|
+        ::Booking::Cache.run(booking_page: booking_page, date: date)
+      end
     end
   end
 end
