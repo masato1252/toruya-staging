47b5931228fcf3ce03de602413c8bb8c68c69df4
ilake <lake.ilakela+github@gmail.com>
2024-10-24 07:29:04 +0800

    Don't cache today's available time

--- app/interactions/booking/available_booking_times.rb
+++ app/interactions/booking/available_booking_times.rb
@@ -27,85 +27,91 @@ module Booking
       json_parsed_date = JSON.parse(raw_special_date)
       special_date = Date.parse(json_parsed_date[START_AT_DATE_PART])
 
+      if special_date.today?
+        process_available_booking_times(special_date)
+      else
+        Rails.cache.fetch(cache_key(special_date), expires_in: 12.hours) do
+          process_available_booking_times(special_date)
+        end
+      end
+    end
+
+    private
+
+    def process_available_booking_times(special_date)
       available_booking_time_mapping = {}
 
-      Rails.cache.fetch(cache_key(special_date), expires_in: 12.hours) do
-        # use empty string avoid return content is nil
+      catch :enough_booking_time do
+        special_dates.each do |raw_special_date|
+          catch :next_working_date do
+            json_parsed_date = JSON.parse(raw_special_date)
+            special_date = Date.parse(json_parsed_date[START_AT_DATE_PART])
 
-        catch :enough_booking_time do
-          special_dates.each do |raw_special_date|
-            catch :next_working_date do
-              json_parsed_date = JSON.parse(raw_special_date)
-              special_date = Date.parse(json_parsed_date[START_AT_DATE_PART])
+            special_date_start_at = Time.zone.parse("#{json_parsed_date[START_AT_DATE_PART]}-#{json_parsed_date[START_AT_TIME_PART]}")
+            special_date_end_at = Time.zone.parse("#{json_parsed_date[END_AT_DATE_PART]}-#{json_parsed_date[END_AT_TIME_PART]}")
 
-              special_date_start_at = Time.zone.parse("#{json_parsed_date[START_AT_DATE_PART]}-#{json_parsed_date[START_AT_TIME_PART]}")
-              special_date_end_at = Time.zone.parse("#{json_parsed_date[END_AT_DATE_PART]}-#{json_parsed_date[END_AT_TIME_PART]}")
+            if special_date < booking_page.available_booking_start_date || special_date > booking_page.available_booking_end_date
+              next
+            end
 
-              if special_date < booking_page.available_booking_start_date || special_date > booking_page.available_booking_end_date
+            shop.user.booking_options.where(id: booking_option_ids).includes(:menus).order("booking_options.id").each do |booking_option|
+              available_booking_times = []
+              booking_start_at = special_date_start_at
+
+              # booking_option doesn't sell on that date
+              if booking_option.start_time.to_date > special_date || booking_option.end_at && booking_option.end_at.to_date < special_date
                 next
               end
 
-              shop.user.booking_options.where(id: booking_option_ids).includes(:menus).order("booking_options.id").each do |booking_option|
-                available_booking_times = []
-                booking_start_at = special_date_start_at
+              if customer && ticket = customer.active_customer_ticket_of_product(booking_option)
+                next if special_date > ticket.expire_at.to_date
+              end
 
-                # booking_option doesn't sell on that date
-                if booking_option.start_time.to_date > special_date || booking_option.end_at && booking_option.end_at.to_date < special_date
-                  next
-                end
+              if booking_page.specific_booking_start_times.present?
+                booking_page.specific_booking_start_times.each do |start_time_time_part|
+                  booking_start_at = Time.zone.parse("#{json_parsed_date[START_AT_DATE_PART]}-#{start_time_time_part}")
+                  booking_end_at = booking_start_at.advance(minutes: booking_option.minutes)
 
-                if customer && ticket = customer.active_customer_ticket_of_product(booking_option)
-                  next if special_date > ticket.expire_at.to_date
-                end
-
-                if booking_page.specific_booking_start_times.present?
-                  booking_page.specific_booking_start_times.each do |start_time_time_part|
-                    booking_start_at = Time.zone.parse("#{json_parsed_date[START_AT_DATE_PART]}-#{start_time_time_part}")
-                    booking_end_at = booking_start_at.advance(minutes: booking_option.minutes)
-
-                    if booking_end_at > special_date_end_at
-                      break
-                    end
-
-                    loop_for_reserable_spot(shop: shop, customer: customer, booking_page: booking_page, booking_option: booking_option, date: booking_start_at.to_date, booking_start_at: booking_start_at, overbooking_restriction: overbooking_restriction) do
-                      available_booking_times << booking_start_at
-
-                      available_booking_time_mapping[booking_start_at] ||= []
-                      available_booking_time_mapping[booking_start_at] << booking_option.id
-
-                      throw :enough_booking_time if limit && available_booking_times.length >= limit
-                    end
+                  if booking_end_at > special_date_end_at
+                    break
                   end
-                else
-                  loop do
-                    booking_end_at = booking_start_at.advance(minutes: booking_option.minutes)
 
-                    if booking_end_at > special_date_end_at
-                      break
-                    end
+                  loop_for_reserable_spot(shop: shop, customer: customer, booking_page: booking_page, booking_option: booking_option, date: booking_start_at.to_date, booking_start_at: booking_start_at, overbooking_restriction: overbooking_restriction) do
+                    available_booking_times << booking_start_at
 
-                    loop_for_reserable_spot(shop: shop, customer: customer, booking_page: booking_page, booking_option: booking_option, date: booking_start_at.to_date, booking_start_at: booking_start_at, overbooking_restriction: overbooking_restriction) do
-                      available_booking_times << booking_start_at
+                    available_booking_time_mapping[booking_start_at] ||= []
+                    available_booking_time_mapping[booking_start_at] << booking_option.id
 
-                      available_booking_time_mapping[booking_start_at] ||= []
-                      available_booking_time_mapping[booking_start_at] << booking_option.id
-
-                      throw :enough_booking_time if limit && available_booking_times.length >= limit
-                    end
-
-                    booking_start_at = booking_start_at.advance(minutes: interval)
+                    throw :enough_booking_time if limit && available_booking_times.length >= limit
                   end
+                end
+              else
+                loop do
+                  booking_end_at = booking_start_at.advance(minutes: booking_option.minutes)
+
+                  if booking_end_at > special_date_end_at
+                    break
+                  end
+
+                  loop_for_reserable_spot(shop: shop, customer: customer, booking_page: booking_page, booking_option: booking_option, date: booking_start_at.to_date, booking_start_at: booking_start_at, overbooking_restriction: overbooking_restriction) do
+                    available_booking_times << booking_start_at
+
+                    available_booking_time_mapping[booking_start_at] ||= []
+                    available_booking_time_mapping[booking_start_at] << booking_option.id
+
+                    throw :enough_booking_time if limit && available_booking_times.length >= limit
+                  end
+
+                  booking_start_at = booking_start_at.advance(minutes: interval)
                 end
               end
             end
           end
         end
-
-        available_booking_time_mapping
       end
-    end
 
-    private
+      available_booking_time_mapping
+    end
 
     def cache_key(date)
       [
