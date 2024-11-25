207ef7d2c425ef6ac7a438baa4981572332ed2c4
ilake <lake.ilakela+github@gmail.com>
2024-11-25 17:12:11 +0800

    Fix locale key name

--- app/jobs/line_profile_job.rb
+++ app/jobs/line_profile_job.rb
@@ -5,7 +5,7 @@ require "line_client"
 class LineProfileJob < ApplicationJob
   SOCIAL_USER_NAME_KEY = "displayName".freeze
   SOCIAL_USER_PICTURE_KEY = "pictureUrl".freeze
-  SOCIAL_USER_LOCALE_KEY = "locale".freeze
+  SOCIAL_USER_LOCALE_KEY = "language".freeze
 
   queue_as :low_priority
 
