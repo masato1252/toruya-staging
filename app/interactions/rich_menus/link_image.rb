d9008d40cfa6b5353238e03fe8a1a303cb6953d3
ilake <lake.ilakela+github@gmail.com>
2025-01-14 00:17:17 +0800

    Fix link image

--- app/interactions/rich_menus/link_image.rb
+++ app/interactions/rich_menus/link_image.rb
@@ -14,32 +14,52 @@ module RichMenus
         file_path: file_url_or_path
       )
 
-      unless response.is_a?(Net::HTTPOK)
-        if social_rich_menu.image.attached?
-          base64_image = Base64.encode64(URI.open(file_url_or_path) { |io| io.read })
-          Rollbar.error("Invalid Rich Menu image", response: response.body, url: file_url_or_path, base64_image: base64_image)
-        else
-          Rollbar.error("Invalid Rich Menu image", response: response.body, url: file_url_or_path)
-        end
-      end
+      return response if response.is_a?(Net::HTTPOK)
 
+      log_error(response)
       response
     end
 
     private
 
     def file_url_or_path
-      if social_rich_menu.image.attached?
-        social_rich_menu.image.url
-      elsif social_rich_menu.social_name.match?(/[0-9]/)
-        if social_account.user.locale == :tw
-          File.join(Rails.root, "app", "assets", "images", "rich_menus", "tw", "#{SocialAccounts::RichMenus::CustomerReservations::KEY}.png")
-        else
-          File.join(Rails.root, "app", "assets", "images", "rich_menus", "#{SocialAccounts::RichMenus::CustomerReservations::KEY}.png")
-        end
+      return social_rich_menu.image.url if social_rich_menu.image.attached?
+
+      build_default_image_path
+    end
+
+    def build_default_image_path
+      filename = if social_rich_menu.social_name.match?(/[0-9]/)
+        SocialAccounts::RichMenus::CustomerReservations::KEY
       else
-        File.join(Rails.root, "app", "assets", "images", "rich_menus", "#{social_rich_menu.social_name}.png")
+        social_rich_menu.social_name
       end
+
+      File.join(
+        Rails.root,
+        "app",
+        "assets",
+        "images",
+        "rich_menus",
+        *locale_path_segment,
+        "#{filename}.png"
+      )
+    end
+
+    def locale_path_segment
+      social_account.user.locale == :tw ? ["tw"] : []
+    end
+
+    def log_error(response)
+      error_data = { response: response.body, url: file_url_or_path }
+      
+      if social_rich_menu.image.attached?
+        error_data[:base64_image] = Base64.encode64(
+          URI.open(file_url_or_path) { |io| io.read }
+        )
+      end
+      
+      Rollbar.error("Invalid Rich Menu image", error_data)
     end
   end
 end
