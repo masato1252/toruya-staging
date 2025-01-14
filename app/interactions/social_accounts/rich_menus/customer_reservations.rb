8487f1426a0abc759240b922ab36fa553c77c9ea
ilake <lake.ilakela+github@gmail.com>
2025-01-14 21:42:40 +0800

    Fix locale for customer dashboard

--- app/interactions/social_accounts/rich_menus/customer_reservations.rb
+++ app/interactions/social_accounts/rich_menus/customer_reservations.rb
@@ -8,60 +8,60 @@ module SocialAccounts
       object :social_account
 
       def execute
-        sentence = I18n.t("line.bot.features.online_booking.rich_menu.bar_text")
-
-        body = {
-          "size": {
-            "width": 2500,
-            "height": 843
-          },
-          "selected": true,
-          "name": KEY,
-          "chatBarText": sentence,
-          "areas": [
-            {
-              "bounds": {
-                "x": 0,
-                "y": 0,
-                "width": 833,
-                "height": 843
-              },
-              "action": {
-                "type": "message",
-                "label": I18n.t("line.bot.keywords.incoming_reservations"),
-                "text": I18n.t("line.bot.keywords.incoming_reservations")
-              }
-            },
-            {
-              "bounds": {
-                "x": 834,
-                "y": 0,
-                "width": 833,
-                "height": 843
-              },
-              "action": {
-                "type": "message",
-                "label": I18n.t("line.bot.keywords.booking_pages"),
-                "text": I18n.t("line.bot.keywords.booking_pages")
-              }
-            },
-            {
-              "bounds": {
-                "x": 1667,
-                "y": 0,
-                "width": 833,
-                "height": 843
-              },
-              "action": {
-                "type": "message",
-                "label": I18n.t("line.bot.keywords.contacts"),
-                "text": I18n.t("line.bot.keywords.contacts")
-              }
-            }
-          ]
-        }
-
         I18n.with_locale(social_account.user.locale) do
+          sentence = I18n.t("line.bot.features.online_booking.rich_menu.bar_text")
+
+          body = {
+            "size": {
+              "width": 2500,
+              "height": 843
+            },
+            "selected": true,
+            "name": KEY,
+            "chatBarText": sentence,
+            "areas": [
+              {
+                "bounds": {
+                  "x": 0,
+                  "y": 0,
+                  "width": 833,
+                  "height": 843
+                },
+                "action": {
+                  "type": "message",
+                  "label": I18n.t("line.bot.keywords.incoming_reservations"),
+                  "text": I18n.t("line.bot.keywords.incoming_reservations")
+                }
+              },
+              {
+                "bounds": {
+                  "x": 834,
+                  "y": 0,
+                  "width": 833,
+                  "height": 843
+                },
+                "action": {
+                  "type": "message",
+                  "label": I18n.t("line.bot.keywords.booking_pages"),
+                  "text": I18n.t("line.bot.keywords.booking_pages")
+                }
+              },
+              {
+                "bounds": {
+                  "x": 1667,
+                  "y": 0,
+                  "width": 833,
+                  "height": 843
+                },
+                "action": {
+                  "type": "message",
+                  "label": I18n.t("line.bot.keywords.contacts"),
+                  "text": I18n.t("line.bot.keywords.contacts")
+                }
+              }
+            ]
+          }
+
           compose(
             ::RichMenus::Create,
             social_account: social_account,
