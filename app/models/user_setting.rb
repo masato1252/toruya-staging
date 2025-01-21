eef4e47ee4e6ff4237c796d597cdddfdd52f0ef6
ilake <lake.ilakela+github@gmail.com>
2025-01-21 11:58:23 +0800

    Don't need customer name by default

--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -14,7 +14,7 @@ class UserSetting < ApplicationRecord
   typed_store :content do |s|
     s.string :line_keyword_booking_page_ids, array: true, default: [], null: false
     s.string :line_keyword_booking_option_ids, array: true, default: [], null: false
-    s.boolean :line_contact_customer_name_required, default: true, null: false
+    s.boolean :line_contact_customer_name_required, default: false, null: false
     s.string :customer_tags, array: true, default: [], null: false
     s.boolean :toruya_message_reply, default: false, null: false
     s.boolean :booking_options_menu_concept, default: true, null: false
