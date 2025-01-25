4518cc257eea15bf3ddb64bc2f59697248567b58
ilake <lake.ilakela+github@gmail.com>
2025-01-25 09:25:11 +0800

    Fix rollbar
    https://app.rollbar.com/a/ilake/fix/item/kasaike/2197?utm_source=rollbar-notification&utm_medium=slack&utm_campaign=new_item_message

--- app/interactions/rich_menus/delete.rb
+++ app/interactions/rich_menus/delete.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require "line_client"
 
 module RichMenus
   class Delete < ActiveInteraction::Base
@@ -8,7 +9,7 @@ module RichMenus
       if Rails.env.development?
         social_rich_menu.destroy
       else
-        response = LineClient.delete_rich_menu(social_rich_menu)
+        response = ::LineClient.delete_rich_menu(social_rich_menu)
 
         if response.is_a?(Net::HTTPOK) || response.is_a?(Net::HTTPNotFound)
           social_rich_menu.image.purge_later if social_rich_menu.image.attached?
