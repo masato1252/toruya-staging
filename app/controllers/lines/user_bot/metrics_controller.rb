4fb92ff2cfab75b7fe7f5154c35d4afd445308a1
ilake <lake.ilakela+github@gmail.com>
2025-03-10 20:06:44 +0800

    Metric in setting page

--- app/controllers/lines/user_bot/metrics_controller.rb
+++ app/controllers/lines/user_bot/metrics_controller.rb
@@ -8,17 +8,19 @@ class Lines::UserBot::MetricsController < Lines::UserBotDashboardController
     comparison_period = metric_start_time.advance(days: -30)..metric_start_time
     @active_customers_rate = Current.business_owner.customers_count.positive? ? ((Current.business_owner.customers.active_in(1.year.ago).count / Current.business_owner.customers_count.to_f) * 100).to_i : 0
 
-    @customers_count = Current.business_owner.customers.where("created_at > ?", metric_start_time).count
+    @customers_count = Current.business_owner.customers.where(created_at: metric_period).count
     @comparison_customers_count = @customers_count - Current.business_owner.customers.where(created_at: comparison_period).count
 
-    @reservations_count = Current.business_owner.reservations.where("created_at > ?", metric_start_time).count
+    @reservations_count = Current.business_owner.reservations.where(created_at: metric_period).count
     @comparison_reservations_count = @reservations_count - Current.business_owner.reservations.where(created_at: comparison_period).count
 
-    @customers_payment = CustomerPayment.completed.where(customer_id: Current.business_owner.customers.select(:id)).where("created_at > ?", metric_start_time).sum(:amount_cents).to_i
-    @comparison_customers_payment = @customers_payment - CustomerPayment.completed.where(customer_id: Current.business_owner.customers.select(:id)).where(created_at: comparison_period).sum(:amount_cents).to_i
     @services_mapping_total_amount = ::Metrics::OnlineServicesRevenues.run!(user: Current.business_owner, metric_period: metric_period)
     @booking_revenue = ::Metrics::BookingRevenue.run!(user: Current.business_owner, metric_period: metric_period)
+    @customers_payment = @services_mapping_total_amount.sum { |service| service[:total_revenue] } + @booking_revenue.sum { |booking| booking["revenue"] }
 
+    @comparison_services_mapping_total_amount = ::Metrics::OnlineServicesRevenues.run!(user: Current.business_owner, metric_period: comparison_period)
+    @comparison_booking_revenue = ::Metrics::BookingRevenue.run!(user: Current.business_owner, metric_period: comparison_period)
+    @comparison_customers_payment = @customers_payment - @comparison_services_mapping_total_amount.sum { |service| service[:total_revenue] } - @comparison_booking_revenue.sum { |booking| booking["revenue"] }
 
     if params[:demo]
       @active_customers_rate = ((rand(100)/100.0) * 100).to_i
--- app/controllers/lines/user_bot/settings_controller.rb
+++ app/controllers/lines/user_bot/settings_controller.rb
@@ -5,10 +5,24 @@ class Lines::UserBot::SettingsController < Lines::UserBotDashboardController
 
   def index
     @subscription = Current.business_owner.subscription
-    @total_customer_count = Current.business_owner.customers.count
-    @total_customer_limit = Plan.max_customers_limit(Current.business_owner.current_plan.level, @subscription.rank) || I18n.t("settings.dashboard.no_limit")
     @social_account = Current.business_owner.social_account
-    @customers_payment = CustomerPayment.completed.where(customer_id: Current.business_owner.customers.select(:id)).where("created_at > ?", metric_start_time).sum(:amount_cents).to_i
+
+    comparison_period = metric_start_time.advance(days: -30)..metric_start_time
+    @active_customers_rate = Current.business_owner.customers_count.positive? ? ((Current.business_owner.customers.active_in(1.year.ago).count / Current.business_owner.customers_count.to_f) * 100).to_i : 0
+
+    @customers_count = Current.business_owner.customers.where(created_at: metric_period).count
+    @comparison_customers_count = @customers_count - Current.business_owner.customers.where(created_at: comparison_period).count
+
+    @reservations_count = Current.business_owner.reservations.where(created_at: metric_period).count
+    @comparison_reservations_count = @reservations_count - Current.business_owner.reservations.where(created_at: comparison_period).count
+
+    @services_mapping_total_amount = ::Metrics::OnlineServicesRevenues.run!(user: Current.business_owner, metric_period: metric_period)
+    @booking_revenue = ::Metrics::BookingRevenue.run!(user: Current.business_owner, metric_period: metric_period)
+    @customers_payment = @services_mapping_total_amount.sum { |service| service[:total_revenue] } + @booking_revenue.sum { |booking| booking["revenue"] }
+
+    @comparison_services_mapping_total_amount = ::Metrics::OnlineServicesRevenues.run!(user: Current.business_owner, metric_period: comparison_period)
+    @comparison_booking_revenue = ::Metrics::BookingRevenue.run!(user: Current.business_owner, metric_period: comparison_period)
+    @comparison_customers_payment = @customers_payment - @comparison_services_mapping_total_amount.sum { |service| service[:total_revenue] } - @comparison_booking_revenue.sum { |booking| booking["revenue"] }
 
     if params[:staff_connect_result].present?
       params[:staff_connect_result] == 'true' ? flash.now[:success] = I18n.t("settings.staff_account.staff_connected_successfully") : flash.now[:alert] = I18n.t("settings.staff_account.staff_connected_failed")
