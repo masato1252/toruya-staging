0346c16559183baf9ccd3b612d5c4879969d92df
ilake <lake.ilakela+github@gmail.com>
2025-03-26 21:49:29 +0800

    tweak migration

--- db/migrate/20250311141530_add_primary_phone_number_and_email_to_customers.rb
+++ db/migrate/20250311141530_add_primary_phone_number_and_email_to_customers.rb
@@ -1,7 +1,7 @@
 class AddPrimaryPhoneNumberAndEmailToCustomers < ActiveRecord::Migration[7.0]
   def change
-    add_column :customers, :customer_email, :string
-    add_column :customers, :customer_phone_number, :string
+    add_column :customers, :customer_email, :string unless column_exists?(:customers, :customer_email)
+    add_column :customers, :customer_phone_number, :string unless column_exists?(:customers, :customer_phone_number)
 
     Customer.find_each do |customer|
       customer.update_columns(
