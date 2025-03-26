2d5d3698038a4923e99fd3dbbdaa66582e385e1c
ilake <lake.ilakela+github@gmail.com>
2025-03-26 21:21:45 +0800

    When customer delete, reset phone and email

--- app/interactions/customers/delete.rb
+++ app/interactions/customers/delete.rb
@@ -6,7 +6,7 @@ class Customers::Delete < ActiveInteraction::Base
 
   def execute
     if soft_delete
-      customer.update_columns(deleted_at: Time.current)
+      customer.update_columns(deleted_at: Time.current, customer_phone_number: nil, customer_email: nil)
       customer.social_customer&.update_columns(customer_id: nil)
       User.reset_counters(customer.user_id, :customers)
     else
