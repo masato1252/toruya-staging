9b72b76d83c1d1651ab20e89f2252b0419713ddd
ilake <lake.ilakela+github@gmail.com>
2025-03-26 21:42:07 +0800

    Reset more

--- app/interactions/customers/delete.rb
+++ app/interactions/customers/delete.rb
@@ -6,7 +6,7 @@ class Customers::Delete < ActiveInteraction::Base
 
   def execute
     if soft_delete
-      customer.update_columns(deleted_at: Time.current, customer_phone_number: nil, customer_email: nil)
+      customer.update_columns(deleted_at: Time.current, customer_phone_number: nil, customer_email: nil, phone_numbers_details: [], emails_details: [])
       customer.social_customer&.update_columns(customer_id: nil)
       User.reset_counters(customer.user_id, :customers)
     else
