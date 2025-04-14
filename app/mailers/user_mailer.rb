3708b2c7ddeec345b6c1f170fecadf64e1048771
ilake <lake.ilakela+github@gmail.com>
2025-04-14 22:43:55 +0800

    Test user mailer layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  layout false
+  layout :determine_layout
 
   def custom
+    @message_content = params[:message]
     mail(
       to: params[:email],
-      subject: params[:subject],
-      body: params[:message]
+      subject: params[:subject]
     )
+  end
+
+  private
+
+  def determine_layout
+    # Check for locale in params or from customer object
+    locale = if params[:locale].present?
+               params[:locale]
+             else
+               I18n.locale || I18n.default_locale
+             end
+
+    case locale.to_s
+    when 'ja'
+      'mailer_ja'
+    when 'tw'
+      'mailer_tw'
+    else
+      'mailer_en'
+    end
   end
 end
