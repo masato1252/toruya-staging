6e8fdb3092b375d01adc88d867d9b19ba4b5baeb
ilake <lake.ilakela+github@gmail.com>
2025-04-15 19:51:39 +0800

    Follow got customer either

--- app/interactions/lines/follow_event.rb
+++ app/interactions/lines/follow_event.rb
@@ -10,5 +10,18 @@ class Lines::FollowEvent < ActiveInteraction::Base
     # Don't send follow message
     # https://toruya.slack.com/archives/C0201K35WMC/p1646709948010259?thread_ts=1646709569.318589&cid=C0201K35WMC
     # LineClient.send(social_customer, I18n.t("line.bot.thanks_follow"))
+    if !social_customer.customer && !social_customer.user.line_contact_customer_name_required
+      if social_customer.social_user_name.blank?
+        LineProfileJob.perform_now(social_customer)
+      end
+
+      customer = compose(
+        Customers::Create,
+        user: social_customer.user,
+        customer_last_name: "",
+        customer_first_name: social_customer.social_user_name
+      )
+      social_customer.update!(customer_id: customer.id)
+    end
   end
 end
