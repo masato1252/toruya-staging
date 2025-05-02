158d743f72b689f5c8cb223ecab7189d9b23668b
ilake <lake.ilakela+github@gmail.com>
2025-05-03 06:55:29 +0800

    Change metric label

--- app/webpacker/javascripts/components/user_bot/metrics/sale_pages_conversions.js
+++ app/webpacker/javascripts/components/user_bot/metrics/sale_pages_conversions.js
@@ -32,7 +32,7 @@ const SalePagesConversionsMetric = ({demo, metric_path, is_phone, page_label_key
             {data.map((sale_page_data) => (
               <React.Fragment key={sale_page_data.label}>
                 <div className="row mb-2">
-                  <div className="col-xs-4">{I18n.t("user_bot.dashboards.metrics.sale_page")}</div>
+                  <div className="col-xs-4">{I18n.t(`user_bot.dashboards.metrics.${page_label_key}`)}</div>
                   <div className="col-xs-8">{sale_page_data.label}</div>
                 </div>
                 <div className="row mb-2">
@@ -40,7 +40,7 @@ const SalePagesConversionsMetric = ({demo, metric_path, is_phone, page_label_key
                   <div className="col-xs-8">{sale_page_data.visit_count}</div>
                 </div>
                 <div className="row mb-2">
-                  <div className="col-xs-4">{I18n.t("user_bot.dashboards.metrics.purchased_count")}</div>
+                  <div className="col-xs-4">{I18n.t(`user_bot.dashboards.metrics.${page_label_key}_purchased_count`)}</div>
                   <div className="col-xs-8">{sale_page_data.purchased_count}</div>
                 </div>
                 <div className="row mb-2">
@@ -65,7 +65,7 @@ const SalePagesConversionsMetric = ({demo, metric_path, is_phone, page_label_key
       <div className="row">
         <div className="col-sm-3 col-xs-12">{I18n.t(`user_bot.dashboards.metrics.${page_label_key}`)}</div>
         <div className="col-sm-1 col-xs-12">{I18n.t("user_bot.dashboards.metrics.visit_count")}</div>
-        <div className="col-sm-1 col-xs-12">{I18n.t("user_bot.dashboards.metrics.purchased_count")}</div>
+        <div className="col-sm-1 col-xs-12">{I18n.t(`user_bot.dashboards.metrics.${page_label_key}_purchased_count`)}</div>
         <div className="col-sm-1 col-xs-12">{I18n.t("user_bot.dashboards.metrics.conversion_rate")}</div>
         <div className="col-sm-1 col-xs-12">{I18n.t("user_bot.dashboards.metrics.total_revenue")}</div>
       </div>
