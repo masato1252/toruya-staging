0e4c96cc05112346ccf0d136dadd05fbc2a4a456
ilake <lake.ilakela+github@gmail.com>
2025-05-08 20:00:40 +0800

    Revert "move line callback out of domain constraint"
    This reverts commit a4d801f212c399f68538958e94134eaf7ba0b675.

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,11 @@ Rails.application.routes.draw do
         get "booking_times"
       end
     end
-  end
 
-  scope module: :lines, path: :lines, as: :lines do
-    scope module: :user_bot, path: :user_bot, as: :user_bot do
-      devise_for :users, :controllers => { omniauth_callbacks: "callbacks", sessions: "users/sessions", passwords: "users/passwords" }
+    scope module: :lines, path: :lines, as: :lines do
+      scope module: :customers, path: :customers, as: :customers do
+        devise_for :users, controllers: { omniauth_callbacks: "lines/user_bot/callbacks", sessions: "lines/user_bot/sessions", passwords: "lines/user_bot/passwords" }
+      end
     end
   end
 
@@ -463,6 +463,7 @@ Rails.application.routes.draw do
       end
     end
 
+    devise_for :users, :controllers => { omniauth_callbacks: "callbacks", sessions: "users/sessions", passwords: "users/passwords" }
 
     resource :member, only: [:show] do
       get "/:reservation_date(/r/:reservation_id)", to: "members#show", on: :collection, constraints: { reservation_date: /\d{4}-\d{1,2}-\d{1,2}/ }, as: :date
