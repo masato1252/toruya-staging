d5fd9015c7244f216446cb5e9f55fc29decf62a0
ilake <lake.ilakela+github@gmail.com>
2025-05-08 18:27:40 +0800

    clean in controller

--- app/controllers/booking_pages_controller.rb
+++ app/controllers/booking_pages_controller.rb
@@ -14,6 +14,7 @@ class BookingPagesController < ActionController::Base
   layout "booking"
 
   def show
+    cookies.clear_across_domains(:oauth_social_account_id, :who)
     if !booking_page.user.subscription.active?
       render inline: t("common.no_service_warning_html")
       return
