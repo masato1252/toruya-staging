d0401c099207e29fc50a9d1b5fa9cb09b02a931e
ilake <lake.ilakela+github@gmail.com>
2025-05-09 14:18:12 +0800

    Remove hardcore line verified message

--- app/interactions/social_messages/create.rb
+++ app/interactions/social_messages/create.rb
@@ -63,8 +63,6 @@ module SocialMessages
 
         # Shop owner customer self send a confirmation message to toruya shop owner user
         if social_customer.is_owner && content == social_user.social_service_user_id && social_customer.customer
-          Notifiers::Users::LineSettings::VerifiedMessage.perform_debounce(receiver: social_user)
-          Notifiers::Users::LineSettings::VerifiedVideo.perform_debounce(receiver: social_user)
           Notifiers::Users::LineSettingsVerified.perform_debounce(receiver: social_user.user)
 
           if social_account.line_settings_verified?
