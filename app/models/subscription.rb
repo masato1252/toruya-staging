0fe745d6b665b0a0c08a50bfe7b9e763e142ead8
ilake <lake.ilakela+github@gmail.com>
2025-05-26 15:38:37 +0800

    Change free plan defintiion

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -87,7 +87,7 @@ class Subscription < ApplicationRecord
   alias_method :in_paid_plan?, :in_paid_plan
 
   def in_free_plan?
-    plan_id == FREE_PLAN_ID
+    current_plan.id == FREE_PLAN_ID
   end
 
   def active?
