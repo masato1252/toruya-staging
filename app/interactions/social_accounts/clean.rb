c77c40a6743dde71d7a4cfe8423fa386c5a62fe0
ilake <lake.ilakela+github@gmail.com>
2025-07-01 14:52:02 +0800

    Always clean the settings

--- app/interactions/social_accounts/clean.rb
+++ app/interactions/social_accounts/clean.rb
@@ -8,18 +8,20 @@ module SocialAccounts
       account = user.social_accounts.first
       outcome = SocialAccounts::RichMenus::SwitchToOfficial.run(social_account: account)
 
-      if outcome.valid?
-        user.social_customers.where(is_owner: true).update_all(is_owner: false, customer_id: nil)
-        account&.update(
-          channel_secret: nil,
-          channel_token: nil,
-          label: nil,
-          login_channel_secret: nil,
-          basic_id: nil,
-          channel_id: nil,
-          login_channel_id: nil
-        )
+      if outcome.invalid?
+        Rollbar.error(outcome.errors.full_messages.join(", "), user_id: user.id)
       end
+
+      user.social_customers.where(is_owner: true).update_all(is_owner: false, customer_id: nil)
+      account&.update(
+        channel_secret: nil,
+        channel_token: nil,
+        label: nil,
+        login_channel_secret: nil,
+        basic_id: nil,
+        channel_id: nil,
+        login_channel_id: nil
+      )
 
       account
     end
