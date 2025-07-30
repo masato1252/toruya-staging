32f08d92e3daff349a4e86f4586b5ece89cccf70
ilake <lake.ilakela+github@gmail.com>
2025-07-30 16:13:22 +0800

    Fix end of date logic

--- app/interactions/schedules/events.rb
+++ app/interactions/schedules/events.rb
@@ -23,8 +23,8 @@ class Schedules::Events < ActiveInteraction::Base
 
     if month_date
       reservations = reservations.in_month(month_date)
-      off_schedules = off_schedules.where("start_time <= ? and end_time >= ?", month_date.end_of_month, month_date.beginning_of_month)
-      open_schedules = open_schedules.where("start_time <= ? and end_time >= ?", month_date.end_of_month, month_date.beginning_of_month)
+      off_schedules = off_schedules.where("start_time <= ? and end_time >= ?", month_date.end_of_month.end_of_day, month_date.beginning_of_month)
+      open_schedules = open_schedules.where("start_time <= ? and end_time >= ?", month_date.end_of_month.end_of_day, month_date.beginning_of_month)
       booking_page_holder_schedules = booking_page_holder_schedules.where("start_at >= ? and end_at <= ?", month_date.beginning_of_month, month_date.end_of_month)
     elsif period_start_date && period_end_date
       reservations = reservations.where("start_time <= ? and start_time >= ?", period_end_date.end_of_day, period_start_date.beginning_of_day)
