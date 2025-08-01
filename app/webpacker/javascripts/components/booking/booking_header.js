5c40a7017427f216599e269fbdf552c55c54b225
ilake <lake.ilakela+github@gmail.com>
2025-08-01 18:19:00 +0800

    Make url clickable in description

--- app/webpacker/javascripts/components/booking/booking_header.js
+++ app/webpacker/javascripts/components/booking/booking_header.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import React from "react";
+import Autolinker from "autolinker";
 
 const BookingHeader = ({booking_page, is_done}) => {
   const {
@@ -9,6 +10,22 @@ const BookingHeader = ({booking_page, is_done}) => {
     shop_logo_url,
     shop_name
   } = booking_page;
+
+  // Function to render greeting with clickable URLs using Autolinker
+  const renderGreetingWithLinks = (text) => {
+    if (!text) return null;
+
+    const linkedText = Autolinker.link(text, {
+      urls: true,
+      email: false,
+      phone: false,
+      newWindow: true,
+      className: '',
+      style: 'color: inherit; text-decoration: underline;'
+    });
+
+    return <div dangerouslySetInnerHTML={{__html: linkedText}} />;
+  };
 
   return (
     <div className="header">
@@ -19,7 +36,7 @@ const BookingHeader = ({booking_page, is_done}) => {
         <h2 className="page-title">{title}</h2>
       </div>
 
-      {!is_done && <div className="greeting">{greeting}</div>}
+      {!is_done && <div className="greeting">{renderGreetingWithLinks(greeting)}</div>}
     </div>
   )
 }
