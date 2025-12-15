a362bf0a97ea3d367ab08e9c5f979da5f19a3e49
masato1252 <noplan.masato125@gmail.com>
2025-12-15 11:37:34 +0900

    Fix OmniAuth callback URL configuration for staging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ OmniAuth.config.full_host = lambda do |env|
   # Use APP_HOST environment variable to set the correct host for callbacks
   # This is especially important for staging/production environments
   scheme = env['rack.url_scheme']
-  host = ENV['APP_HOST'] || env['HTTP_HOST']
+  host = ENV['HEROKU_APP_DEFAULT_DOMAIN_NAME'] || env['HTTP_HOST']
   "#{scheme}://#{host}"
 end
 
