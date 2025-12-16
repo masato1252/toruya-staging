87cf99784c7367c1c8d2c9a38e775d57cb2ef917
masato1252 <noplan.masato125@gmail.com>
2025-12-16 15:42:34 +0900

    予約向け宣伝LPを独自日時設定可能に

--- app/serializers/sale_pages/booking_page_serializer.rb
+++ app/serializers/sale_pages/booking_page_serializer.rb
@@ -5,11 +5,13 @@ class SalePages::BookingPageSerializer < SalePageSerializer
   attribute :product do |object, params|
     booking_page_attrs = ::BookingPageSerializer.new(object.product, params: params).attributes_hash
     
-    # Override is_started and is_ended to use SalePage's selling period instead of BookingPage's period
+    # Override is_started, is_ended, start_time, and end_time to use SalePage's selling period instead of BookingPage's period
     booking_page_attrs.merge!(
       url: Rails.application.routes.url_helpers.booking_page_url(object.product.slug, _from: "sale_page", _from_id: object.id, function_access_id: params[:function_access_id]),
       is_started: object.started?,
-      is_ended: object.ended?
+      is_ended: object.ended?,
+      start_time: object.start_time_text,
+      end_time: object.end_time_text
     )
   end
 
